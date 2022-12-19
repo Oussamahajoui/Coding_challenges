1d7ee070ff7d4adb01c7d66dd5838c51a7ba3b8b
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2022-12-19 06:43:33 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Longest vowel chain.ts
+++ Codewars/Typescript/7_kyu/Longest vowel chain.ts
@@ -4,3 +4,17 @@
 // Good luck!
 
 // My Solution:
+export function solve(s: string) {
+  const vowels: string[] = ["a", "e", "i", "o", "u"];
+  let res: number = 0;
+  let sofar: number = 0;
+  for (const c of s) {
+    if (vowels.includes(c)) {
+      sofar += 1;
+      res = Math.max(sofar, res);
+    } else {
+      sofar = 0;
+    }
+  }
+  return res;
+}
