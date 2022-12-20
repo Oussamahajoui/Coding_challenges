ff9fdbae6003b47b3614f4b8dd3bff34a69bbac2
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2022-12-20 09:05:32 -0500

    added my solution

--- Codewars/Typescript/7_kyu/All Inclusive.ts
+++ Codewars/Typescript/7_kyu/All Inclusive.ts
@@ -20,3 +20,11 @@
 // and for any array arr: contain_all_rots("", arr) --> true
 
 // My Solution:
+export function containAllRots(str: string, arr: string[]): boolean {
+  for (let i = 0; i < str.length; i++) {
+    if (arr.indexOf(str.slice(i) + str.slice(0, i)) === -1) {
+      return false;
+    }
+  }
+  return true;
+}
