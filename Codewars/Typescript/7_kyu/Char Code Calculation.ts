9216240e9198069b163a81459309b2c94d6fa252
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2022-12-22 09:24:38 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Char Code Calculation.ts
+++ Codewars/Typescript/7_kyu/Char Code Calculation.ts
@@ -16,3 +16,20 @@
 //                        6
 
 // My Solution:
+export const calc = (x: string): number => {
+  let total1: string = "";
+  for (let i = 0; i < x.length; i++) {
+    total1 += x.charCodeAt(i);
+  }
+  let total2: string = total1.replaceAll("7", "1");
+  let res: number =
+    total1
+      .split("")
+      .map((e) => parseFloat(e))
+      .reduce((a, b) => a + b, 0) -
+    total2
+      .split("")
+      .map((e) => parseFloat(e))
+      .reduce((a, b) => a + b, 0);
+  return res;
+};
