44b84b084c0d01b8d3a4aa60ad2bfdedd9278e5f
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2022-12-28 09:37:39 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Ordered Count of Characters.ts
+++ Codewars/Typescript/7_kyu/Ordered Count of Characters.ts
@@ -8,3 +8,15 @@
 // orderedCount("abracadabra") == [['a', 5], ['b', 2], ['r', 2], ['c', 1], ['d', 1]]
 
 // My Solution:
+export function orderedCount(text: string): [string, number][] {
+  let res: [string, number][] = [];
+  let visited: Set<string> = new Set();
+  for (let c of text) {
+    if (!visited.has(c)) {
+      visited.add(c);
+      let count: number = text.split("").filter((e) => e === c).length;
+      res.push([c, count]);
+    }
+  }
+  return res;
+}
