44b1740a5f1c371ba8fd638417fb18b05a6926da
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-01-08 14:40:50 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Factorial.ts
+++ Codewars/Typescript/7_kyu/Factorial.ts
@@ -4,3 +4,14 @@
 // https://en.wikipedia.org/wiki/Factorial
 
 // My Solution:
+export function factorial(n: number) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let res: number = 1;
+  while (n > 0) {
+    res *= n;
+    n -= 1;
+  }
+  return res;
+}
