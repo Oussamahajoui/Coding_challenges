f4f3ed80c30ca4619da567f6342a8dca6c190e13
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-01-16 09:05:38 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Sum of angles.ts
+++ Codewars/Typescript/7_kyu/Sum of angles.ts
@@ -2,3 +2,6 @@
 // Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2.
 
 // My Solution:
+export function angle(n: number): number {
+  return (n - 2) * 180;
+}
