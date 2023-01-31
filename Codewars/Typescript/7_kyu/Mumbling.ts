0c7a94e87f35482e6133129bcf336bc37cb10343
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-01-31 09:25:25 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Mumbling.ts
+++ Codewars/Typescript/7_kyu/Mumbling.ts
@@ -8,3 +8,10 @@
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 // My Solution:
+export function accum(s: string): string {
+  let res: string[] = [];
+  for (let i = 0; i < s.length; i++) {
+    res.push(s[i].toUpperCase() + s[i].toLowerCase().repeat(i));
+  }
+  return res.join("-");
+}
