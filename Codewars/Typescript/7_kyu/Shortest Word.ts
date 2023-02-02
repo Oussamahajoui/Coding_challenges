c4596fe2f0af6700d9f830da2c2462a020156f89
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-02 10:31:23 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Shortest Word.ts
+++ Codewars/Typescript/7_kyu/Shortest Word.ts
@@ -4,3 +4,10 @@
 // String will never be empty and you do not need to account for different data types.
 
 // My Solution:
+export function findShort(s: string): number {
+  let res: number = Number.POSITIVE_INFINITY;
+  for (const c of s.split(" ")) {
+    res = Math.min(c.length, res);
+  }
+  return res;
+}
