18e1cd526e2a8c5e40c15e9f2e04a196def77ecb
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-04 10:23:07 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Get the Middle Character.ts
+++ Codewars/Typescript/7_kyu/Get the Middle Character.ts
@@ -10,3 +10,15 @@
 // "A" --> "A"
 
 // My Solution:
+export function getMiddle(s: string) {
+  const N: number = s.length;
+  if (N % 2 === 1) {
+    // odd
+    let idx: number = Math.floor(N / 2);
+    return s[idx];
+  } else {
+    // even case
+    let idx = N / 2;
+    return s.slice(idx - 1, idx + 1);
+  }
+}
