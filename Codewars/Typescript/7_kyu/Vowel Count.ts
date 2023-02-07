3c2414b1d612b609af985bde457a88f60352b57e
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-07 09:19:59 -0500

    added my solution

--- Codewars/Typescript/7_kyu/Vowel Count.ts
+++ Codewars/Typescript/7_kyu/Vowel Count.ts
@@ -6,3 +6,15 @@
 // The input string will only consist of lower case letters and/or spaces.
 
 // My Solution:
+export class Kata {
+  static getCount(str: string): number {
+    const vowels: string[] = ["a", "e", "i", "o", "u"];
+    let res: number = 0;
+    for (const c of str) {
+      if (vowels.includes(c)) {
+        res += 1;
+      }
+    }
+    return res;
+  }
+}
