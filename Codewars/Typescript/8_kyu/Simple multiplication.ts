cd812f550d3001c5390bac6c5e8ee7e75cb28de8
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-14 09:02:06 -0500

    added my solution

--- Codewars/Typescript/8_kyu/Simple multiplication.ts
+++ Codewars/Typescript/8_kyu/Simple multiplication.ts
@@ -2,3 +2,6 @@
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 // My Solution:
+export function simpleMultiplication(num: number): number {
+  return num % 2 ? num * 9 : num * 8;
+}
