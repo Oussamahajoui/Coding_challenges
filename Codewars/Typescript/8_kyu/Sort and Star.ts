386f359fe237bdafced2c002d9cc81c61d330aab
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-18 09:07:16 -0500

    added my solution

--- Codewars/Typescript/8_kyu/Sort and Star.ts
+++ Codewars/Typescript/8_kyu/Sort and Star.ts
@@ -6,3 +6,13 @@
 // You should not remove or add elements from/to the array.
 
 // My Solution:
+export function twoSort(s: string[]): string {
+  s.sort();
+  let first = s[0];
+  let res: string[] = [];
+  for (let c of first) {
+    res.push(c);
+    res.push("***");
+  }
+  return res.slice(0, res.length - 1).join("");
+}
