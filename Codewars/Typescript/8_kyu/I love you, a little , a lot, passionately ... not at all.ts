3c67d70b4fe1cd2f35e4d5f8279e72d154885047
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-19 09:19:56 -0500

    added my solution

--- Codewars/Typescript/8_kyu/I love you, a little , a lot, passionately ... not at all.ts
+++ Codewars/Typescript/8_kyu/I love you, a little , a lot, passionately ... not at all.ts
@@ -14,3 +14,22 @@
 // Your goal in this kata is to determine which phrase the girls would say at the last petal for a flower of a given number of petals. The number of petals is always greater than 0.
 
 // My Solution:
+export function howMuchILoveYou(petals: number): string {
+  let res: number = petals % 6;
+  switch (res) {
+    case 0:
+      return "not at all";
+    case 1:
+      return "I love you";
+    case 2:
+      return "a little";
+    case 3:
+      return "a lot";
+    case 4:
+      return "passionately";
+    case 5:
+      return "madly";
+    default:
+      return "Nevermind....";
+  }
+}
