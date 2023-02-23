6302d0da6ef0126f48f96239fef0fd431a54d500
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-23 09:23:50 -0500

    added my solution

--- Codewars/Typescript/8_kyu/Cat years, Dog years.ts
+++ Codewars/Typescript/8_kyu/Cat years, Dog years.ts
@@ -19,3 +19,22 @@
 // +5 dog years for each year after that
 
 // My Solution:
+export function humanYearsCatYearsDogYears(
+  humanYears: number
+): [number, number, number] {
+  let catYears = 0;
+  let dogYears = 0;
+  for (let i = 1; i <= humanYears; i++) {
+    if (i === 1) {
+      catYears += 15;
+      dogYears += 15;
+    } else if (i === 2) {
+      catYears += 9;
+      dogYears += 9;
+    } else {
+      catYears += 4;
+      dogYears += 5;
+    }
+  }
+  return [humanYears, catYears, dogYears];
+}
