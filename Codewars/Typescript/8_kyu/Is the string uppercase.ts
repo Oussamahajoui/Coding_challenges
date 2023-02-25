9127d77af4eaa35af9227c58694845af2b6f2350
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-02-25 09:21:15 -0500

    added my solution

--- Codewars/Typescript/8_kyu/Is the string uppercase.ts
+++ Codewars/Typescript/8_kyu/Is the string uppercase.ts
@@ -13,3 +13,6 @@
 // In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 
 // My Solution:
+export function isUpperCase(str: string) {
+  return str === str.toUpperCase();
+}
