998b630fbf2fdd902980b5384bb5a812e026ae81
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-02 09:43:40 -0500

    added my solution

--- Codewars/Typescript/8_kyu/Keep up the hoop.ts
+++ Codewars/Typescript/8_kyu/Keep up the hoop.ts
@@ -7,3 +7,8 @@
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
 // My Solution:
+export function hoopCount(n: number): string {
+  return n >= 10
+    ? "Great, now move on to tricks"
+    : "Keep at it until you get it";
+}
