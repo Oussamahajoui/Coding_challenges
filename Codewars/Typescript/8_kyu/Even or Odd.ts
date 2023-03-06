b7294f47187c902c4a88ac2950662aa402605ac7
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-06 09:06:21 -0500

    added my solution

--- Codewars/Typescript/8_kyu/Even or Odd.ts
+++ Codewars/Typescript/8_kyu/Even or Odd.ts
@@ -2,3 +2,10 @@
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 // My Solution:
+export function evenOrOdd(n: number): string {
+  if (n % 2 !== 0) {
+    return "Odd";
+  } else {
+    return "Even";
+  }
+}
