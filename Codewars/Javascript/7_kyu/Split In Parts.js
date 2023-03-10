58eb909654634e34d14acf73c73e19e79c01ed12
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-10 10:02:56 -0500

    added my solution

--- Codewars/Javascript/7_kyu/Split In Parts.js
+++ Codewars/Javascript/7_kyu/Split In Parts.js
@@ -16,3 +16,10 @@
 // Size is always positive
 
 // My Solution:
+var splitInParts = function (s, partLength) {
+  let arr = [];
+  for (let i = 0; i < s.length; i += partLength) {
+    arr.push(s.slice(i, i + partLength));
+  }
+  return arr.join(" ");
+};
