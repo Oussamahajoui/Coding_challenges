c52eee11ec8edee9bdd32cc271a85265c01ae5b6
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-20 10:58:33 -0400

    added today's problem

---
+++ Codewars/Javascript/7_kyu/Changing letters.js
@@ -0,0 +1,12 @@
+// Description:
+// When provided with a String, capitalize all vowels
+
+// For example:
+
+// Input : "Hello World!"
+
+// Output : "HEllO WOrld!"
+
+// Note: Y is not a vowel in this kata.
+
+// My Solution:
