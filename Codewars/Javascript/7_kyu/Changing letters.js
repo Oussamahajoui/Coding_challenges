5f720235f8017d2fb4bbd9457d0a67ea88639747
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-20 11:00:49 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Changing letters.js
+++ Codewars/Javascript/7_kyu/Changing letters.js
@@ -10,3 +10,15 @@
 // Note: Y is not a vowel in this kata.
 
 // My Solution:
+function swap(string) {
+  const vowels = ["a", "e", "i", "o", "u"];
+  let res = "";
+  for (const c of string) {
+    if (vowels.includes(c.toLowerCase())) {
+      res += c.toUpperCase();
+    } else {
+      res += c;
+    }
+  }
+  return res;
+}
