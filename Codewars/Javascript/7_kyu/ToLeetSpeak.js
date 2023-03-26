f4fbdb45a3dcbf83f73f403d81004f0f3bc13980
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-26 10:46:04 -0400

    added my solution

--- Codewars/Javascript/7_kyu/ToLeetSpeak.js
+++ Codewars/Javascript/7_kyu/ToLeetSpeak.js
@@ -40,3 +40,40 @@
 // }
 
 // My Solution:
+function toLeetSpeak(str) {
+  const leet = {
+    A: "@",
+    B: "8",
+    C: "(",
+    D: "D",
+    E: "3",
+    F: "F",
+    G: "6",
+    H: "#",
+    I: "!",
+    J: "J",
+    K: "K",
+    L: "1",
+    M: "M",
+    N: "N",
+    O: "0",
+    P: "P",
+    Q: "Q",
+    R: "R",
+    S: "$",
+    T: "7",
+    U: "U",
+    V: "V",
+    W: "W",
+    X: "X",
+    Y: "Y",
+    Z: "2",
+    " ": " ",
+  };
+
+  let res = "";
+  for (const c of str) {
+    res += leet[c];
+  }
+  return res;
+}
