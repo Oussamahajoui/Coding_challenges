bafc5542fb3c292ae1cb888f8d4edc2d851d9970
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-29 10:03:19 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Regexp Basics - is it a letter.js
+++ Codewars/Javascript/7_kyu/Regexp Basics - is it a letter.js
@@ -2,3 +2,6 @@
 // Complete the code which should return true if the given object is a single ASCII letter (lower or upper case), false otherwise.
 
 // My Solution:
+String.prototype.isLetter = function () {
+  return /^[a-zA-Z]$/.test(this);
+};
