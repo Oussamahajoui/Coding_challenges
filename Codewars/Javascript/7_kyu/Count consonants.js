69b6908b46eb08dd026664186706d946e57e7169
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-03-30 14:58:31 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Count consonants.js
+++ Codewars/Javascript/7_kyu/Count consonants.js
@@ -4,3 +4,35 @@
 // Consonants are all letters used to write English excluding the vowels a, e, i, o, u.
 
 // My Solution:
+function consonantCount(str) {
+  const vowelsAndSpecial = [
+    "a",
+    "e",
+    "i",
+    "o",
+    "u",
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "^",
+    "&",
+    "$",
+    "#",
+    "_",
+    " ",
+  ];
+  let res = 0;
+  for (const c of str) {
+    if (!vowelsAndSpecial.includes(c.toLowerCase())) {
+      res++;
+    }
+  }
+  return res;
+}
