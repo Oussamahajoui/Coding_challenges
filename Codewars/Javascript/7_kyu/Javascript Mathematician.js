a2b6eb86fa988fc8bb87a4fb2d4ac240535c4146
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-04-03 10:32:42 -0400

    added today's problem + solution

---
+++ Codewars/Javascript/7_kyu/Javascript Mathematician.js
@@ -0,0 +1,17 @@
+// Description:
+// You are writing a function that takes two sets of arguments of arbitrary length. The return value will be the sum of the values of all of the arguments.
+
+// The function should contain at least 1 argument per set.
+
+// calculate(1)(1) // should return 2
+// calculate(1,1)(1) // should return 3
+// calculate(1,1)(1,-1) // should return 2
+// calculate(2,4)(3,7,1) // should return 17
+
+// My Solution:
+function calculate(...args1) {
+  return function (...args2) {
+    const nums = [...args1, ...args2];
+    return nums.reduce((a, b) => a + b, 0);
+  };
+}
