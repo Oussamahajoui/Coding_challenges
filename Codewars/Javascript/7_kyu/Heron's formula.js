5229fc2075353c6257465f33e9e231c71b8d48ea
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-04-08 10:08:15 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Heron's formula.js
+++ Codewars/Javascript/7_kyu/Heron's formula.js
@@ -1,6 +1,12 @@
 // Description:
 // Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
- 
+
 // Output should have 2 digits precision.
 
 // My Solution:
+function heron(a, b, c) {
+    const s = (a + b + c) / 2
+    const heron = s * (s - a) * (s - b) * (s - c)
+    const res = parseFloat(Math.sqrt(heron).toFixed(2))
+    return res
+}
