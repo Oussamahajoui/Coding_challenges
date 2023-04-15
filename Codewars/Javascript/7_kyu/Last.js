ee81afbbda9528b6d9708638d4c65e18327a9aca
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-04-15 10:23:21 -0400

    added today's problem

---
+++ Codewars/Javascript/7_kyu/Last.js
@@ -0,0 +1,8 @@
+// Description:
+// Find the last element of the given argument(s). If a single argument is passed and is a list/array or a string, return its last element. It is guaranteed that there will be at least one argument and that single-argument arrays/lists/strings will not be empty.
+
+// Examples
+// last [1, 2, 3, 4]     -- =>  4
+// last ['x', 'y', 'z']  -- => 'z'
+
+// My Solution:
