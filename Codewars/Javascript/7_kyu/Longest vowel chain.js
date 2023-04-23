5a6f240362f46a2bcf3adac90dec6e0f6ad134e1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-04-23 10:40:11 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Longest vowel chain.js
+++ Codewars/Javascript/7_kyu/Longest vowel chain.js
@@ -4,3 +4,17 @@
 // Good luck!
 
 // My Solution:
+function solve(s) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let res = 0
+    let sofar = 0
+    for (const c of s) {
+        if (vowels.includes(c)) {
+            sofar += 1
+            res = Math.max(sofar, res)
+        } else {
+            sofar = 0
+        }
+    }
+    return res
+}
