34c80e52bedfef12496ea03eb0d241419f841741
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-04-27 10:35:33 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Sort by Last Char.js
+++ Codewars/Javascript/7_kyu/Sort by Last Char.js
@@ -6,3 +6,15 @@
 // All inputs will be valid.
 
 // My Solution:
+function last(x) {
+    return x.split(' ').sort((a, b) => {
+        const lastCharA = a[a.length - 1];
+        const lastCharB = b[b.length - 1];
+        return lastCharA.localeCompare(lastCharB)
+    })
+}
+
+//Other way:
+function last(x) {
+    return x.split(' ').sort((a, b) => a.charCodeAt(a.length - 1) - b.charCodeAt(b.length - 1));
+}
