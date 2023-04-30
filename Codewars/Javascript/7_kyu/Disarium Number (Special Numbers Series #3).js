141894e35cbe837f1e5a9fb4cf9d130954eb2f89
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-04-30 10:14:08 -0400

    added today's problem

---
+++ Codewars/Javascript/7_kyu/Disarium Number (Special Numbers Series #3).js
@@ -0,0 +1,21 @@
+// Description:
+// Definition
+// Disarium number is the number that The sum of its digits powered with their respective positions is equal to the number itself.
+
+// Task
+// Given a number, Find if it is Disarium or not .
+
+// Warm-up (Highly recommended)
+// Playing With Numbers Series
+// Notes
+// Number passed is always Positive .
+// Return the result as String
+// Input >> Output Examples
+// disariumNumber(89) ==> return "Disarium !!"
+// Explanation:
+// Since , 8^1 + 9^2 = 89 , thus output is "Disarium !!"
+// disariumNumber(564) ==> return "Not !!"
+// Explanation:
+// Since , 5^1 + 6^2 + 4^3 = 10^5 != 564 , thus output is "Not !!"
+
+// My Solution:
