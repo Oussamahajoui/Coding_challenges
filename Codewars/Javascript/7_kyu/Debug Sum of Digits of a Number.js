520506d1d70ddf7da2e3ee7bf8433b29b17f982a
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-02 10:26:56 -0400

    added today's problem

---
+++ Codewars/Javascript/7_kyu/Debug Sum of Digits of a Number.js
@@ -0,0 +1,9 @@
+// Description:
+// Debug   function getSumOfDigits that takes positive integer to calculate sum of its digits. Assume that argument is an integer.
+
+// Example
+// 123  => 6
+// 223  => 7
+// 1337 => 14
+
+// My Solution:
