3659e6808d66bbb6f5e9afedb1f8a53209ff0931
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-02 10:29:55 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Debug Sum of Digits of a Number.js
+++ Codewars/Javascript/7_kyu/Debug Sum of Digits of a Number.js
@@ -7,3 +7,11 @@
 // 1337 => 14
 
 // My Solution:
+function getSumOfDigits(integer) {
+    let sum = null;
+    let digits = integer.toString().split('');
+    for (let ix = 0; ix < digits.length; ix++) {
+        sum += parseInt(digits[ix]);
+    }
+    return sum;
+}
