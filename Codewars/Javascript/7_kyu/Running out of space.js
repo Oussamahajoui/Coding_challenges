4c940736d46abf98e05203578de005d54419f7f1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-03 10:26:22 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Running out of space.js
+++ Codewars/Javascript/7_kyu/Running out of space.js
@@ -3,3 +3,19 @@
 // For example, running this function on the array ['i', 'have','no','space'] would produce ['i','ihave','ihaveno','ihavenospace']
 
 // My Solution:
+function spacey(array) {
+    return array.map((e, i) => {
+        if (i > 0) {
+            return array.slice(0, i).join('') + e
+        } else {
+            return e
+        }
+    }
+    )
+}
+
+// Better way:
+function spacey(array) {
+    let string = ''
+    return array.map((e) => string += e)
+}
