d24b470dbbb3efa94b839806e4f1daaa90f400b4
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-13 10:25:41 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Get key/value pairs as arrays.js
+++ Codewars/Javascript/7_kyu/Get key/value pairs as arrays.js
@@ -7,3 +7,16 @@
 // Style Points (JS/CoffeeScript only): This kata only tests for data that uses object literal notation (simple objects). For extra style, can you get your method to check for objects that extend their prototype?
 
 // My Solution:
+function keysAndValues(data) {
+    let keys = [], values = []
+    for (const [key, value] of Object.entries(data)) {
+        keys.push(key)
+        values.push(value)
+    }
+    return [keys, values]
+}
+
+// Better way:
+function keysAndValues(data) {
+    return [Object.keys(data), Object.values(data)];
+}
