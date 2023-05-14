e2595353914d436dd60f811eb4b728f39cabe0a1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-14 10:16:22 -0400

    added today's problem

---
+++ Codewars/Javascript/7_kyu/esreveR.js
@@ -0,0 +1,6 @@
+// Description:
+// Write a function reverse which reverses a list (or in clojure's case, any list-like data structure)
+
+// (the dedicated builtin(s) functionalities are deactivated)
+
+// My Solution:
