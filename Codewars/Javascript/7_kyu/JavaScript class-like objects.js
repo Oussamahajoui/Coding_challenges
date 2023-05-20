b2a369aff0e5b0ab3362c2317cfe679662e44dca
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-20 10:18:16 -0400

    added my solution

--- Codewars/Javascript/7_kyu/JavaScript class-like objects.js
+++ Codewars/Javascript/7_kyu/JavaScript class-like objects.js
@@ -10,3 +10,13 @@
 // dog.name = 'Lassie'; // should set name to 'Lassie'
 
 // My Solution:
+// create your Animal class like object here
+class Animal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    toString() {
+        return `${this.name} is a ${this.type}`
+    }
+}
