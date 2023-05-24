19f1f6dc639f55503aa72f57065f81380f9f058d
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-24 11:54:59 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Alternate case.js
+++ Codewars/Javascript/7_kyu/Alternate case.js
@@ -2,3 +2,25 @@
 // Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
 
 // My Solution:
+function alternateCase(s) {
+    let res = ""
+    for (let c of s) {
+        if ('a' <= c && c <= 'z') {
+            c = c.toUpperCase()
+            res += c
+        } else if ('A' <= c && c <= 'Z') {
+            c = c.toLowerCase()
+            res += c
+        } else {
+            res += c
+        }
+    }
+    return res
+}
+
+// Less verbose way:
+function alternateCase(s) {
+    return s.split('').map(function (el) {
+        return el === el.toUpperCase() ? el.toLowerCase() : el.toUpperCase();
+    }).join('');
+}
