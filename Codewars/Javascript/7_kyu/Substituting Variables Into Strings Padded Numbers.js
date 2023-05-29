86eeb7238a55acdc4de00aa22acc207ad68d47bd
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-05-29 10:47:21 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Substituting Variables Into Strings Padded Numbers.js
+++ Codewars/Javascript/7_kyu/Substituting Variables Into Strings Padded Numbers.js
@@ -6,3 +6,18 @@
 // solution(5) // should return "Value is 00005"
 
 // My Solution:
+function solution(value) {
+    let res = ''
+    let N = String(value).length
+    if (N < 5) {
+        let zeros = (5 - N)
+        for (let i = 0; i < zeros; i++) {
+            res += '0'
+        }
+    }
+    res += value
+    return `Value is ${res}`
+}
+
+//Other way:
+const solution = value => 'Value is ' + value.toString().padStart(5, '0');
