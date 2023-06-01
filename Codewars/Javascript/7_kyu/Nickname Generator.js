d0858e8b8bfe463259de39733eecb78ad7a28758
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-06-01 10:29:30 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Nickname Generator.js
+++ Codewars/Javascript/7_kyu/Nickname Generator.js
@@ -23,3 +23,13 @@
 // The input can be modified
 
 // My Solution:
+function nicknameGenerator(name) {
+    if (name.length < 4) {
+        return "Error: Name too short"
+    }
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    if (vowels.includes(name[2])) {
+        return name.slice(0, 4)
+    }
+    return name.slice(0, 3)
+}
