9fec0820da3817bef59ac5b6c6b0bb174bef01c4
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-06-05 09:56:21 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Building blocks.js
+++ Codewars/Javascript/7_kyu/Building blocks.js
@@ -18,16 +18,39 @@
 // Examples
 //     let b = new Block([2,4,6]) -> creates a `Block` object with a width of `2` a length of `4` and a height of `6`
 //     b.getWidth() // -> 2
-    
+
 //     b.getLength() // -> 4
-    
+
 //     b.getHeight() // -> 6
-    
+
 //     b.getVolume() // -> 48
-    
+
 //     b.getSurfaceArea() // -> 88
 // Note: no error checking is needed
 
 // Any feedback would be much appreciated
 
 // My Solution:
+class Block {
+
+    constructor(data) {
+        [this.width, this.length, this.height] = [...data]
+    }
+
+    getWidth() {
+        return this.width
+    }
+    getLength() {
+        return this.length
+    }
+    getHeight() {
+        return this.height
+    }
+    getVolume() {
+        return this.width * this.length * this.height
+    }
+    getSurfaceArea() {
+        return 2 * (this.width * this.length) + 2 * (this.width * this.height) + 2 * (this.height * this.length)
+    }
+
+}
