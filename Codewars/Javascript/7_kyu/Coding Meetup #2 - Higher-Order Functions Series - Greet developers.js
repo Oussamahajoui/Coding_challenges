c99b22a618d27811d746a4968387f678bd9c1782
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-06-06 10:57:49 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Coding Meetup #2 - Higher-Order Functions Series - Greet developers.js
+++ Codewars/Javascript/7_kyu/Coding Meetup #2 - Higher-Order Functions Series - Greet developers.js
@@ -36,3 +36,7 @@
 // This kata is part of the Coding Meetup series which includes a number of short and easy to follow katas which have been designed to allow mastering the use of higher-order functions. In JavaScript this includes methods like: forEach, filter, map, reduce, some, every, find, findIndex. Other approaches to solving the katas are of course possible.
 
 // My Solution:
+function greetDevelopers(list) {
+    let newList = list.map(e => { return { ...e, greeting: `Hi ${e.firstName}, what do you like the most about ${e.language}?` } })
+    return newList
+}
