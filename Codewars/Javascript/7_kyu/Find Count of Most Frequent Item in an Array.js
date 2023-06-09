fa99858a82d43077c9fb34df400bf6365742f5e4
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-06-09 10:44:46 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Find Count of Most Frequent Item in an Array.js
+++ Codewars/Javascript/7_kyu/Find Count of Most Frequent Item in an Array.js
@@ -7,3 +7,26 @@
 // The most frequent number in the array is -1 and it occurs 5 times.
 
 // My Solution:
+function mostFrequentItemCount(collection) {
+    if (collection.length < 1) {
+        return 0
+    }
+    let counter = {}
+    for (let i = 0; i < collection.length; i++) {
+        if (collection[i] in counter) {
+            counter[collection[i]] += 1
+        } else {
+            counter[collection[i]] = 1
+        }
+    }
+    let res = 0
+    for (const [key, value] of Object.entries(counter)) {
+        res = Math.max(value, res)
+    }
+    return res
+}
+
+// More JS way:
+function mostFrequentItemCount(c) {
+    return c.length ? Math.max(...c.map(x => c.filter(y => y == x).length)) : 0;
+}
