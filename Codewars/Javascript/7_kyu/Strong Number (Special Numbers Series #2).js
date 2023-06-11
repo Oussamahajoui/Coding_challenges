02af6bf3a935200101bc5e1ffb31d083206257cf
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-06-11 10:48:01 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Strong Number (Special Numbers Series #2).js
+++ Codewars/Javascript/7_kyu/Strong Number (Special Numbers Series #2).js
@@ -13,3 +13,30 @@
 // 150 is not a strong number, because 1! + 5! + 0! = 122 is not equal to 150, so return "Not Strong !!".
 
 // My Solution:
+function strong(n) {
+    const cache = {}
+    function factorize(x) {
+        if (x in cache) {
+            return cache[x]
+        }
+
+        if (x < 0) {
+            return -1
+        }
+        if (x === 0 || x === 1) {
+            return 1
+        }
+
+        const res = factorize(x - 1) * x
+        cache[x] = res
+        return res
+    }
+    const originalNum = n
+    let value = 0
+    while (n > 0) {
+        let digit = n % 10
+        value += factorize(digit)
+        n = Math.floor(n / 10)
+    }
+    return value === originalNum ? "STRONG!!!!" : "Not Strong !!";
+}
