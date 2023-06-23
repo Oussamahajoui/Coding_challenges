c32ad567a0e5f6a239e9137d857f89be83c1be86
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-06-23 10:18:26 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Reverse a Number.js
+++ Codewars/Javascript/7_kyu/Reverse a Number.js
@@ -9,3 +9,25 @@
 // 1000 ->    1
 
 // My Solution:
+function reverseNumber(n) {
+    let arrN = String(n).split('')
+    let res = ""
+    if (arrN[0] === '-') {
+        res = "-"
+    }
+    for (let i = arrN.length - 1; i >= 0; i--) {
+        if (arrN[i] >= '0' && arrN[i] <= '9') {
+            res += arrN[i]
+        }
+    }
+    return parseFloat(res)
+}
+
+// Better way:
+function reverseNumber(n) {
+    let isNegative = n < 0;
+    let reverseAsString = Math.abs(n).toString().split('').reverse().join('');
+    let result = Number(reverseAsString);
+
+    return isNegative ? -result : result;
+}
