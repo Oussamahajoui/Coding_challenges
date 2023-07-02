5ca46a49406b1732b0fb8712306b339b7ef4f21b
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-07-02 13:33:08 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Divide and Conquer.js
+++ Codewars/Javascript/7_kyu/Divide and Conquer.js
@@ -4,3 +4,8 @@
 // Return as a number.
 
 // My Solution:
+function divCon(x) {
+    let stringNums = x.filter((e) => typeof e !== 'number').reduce((a, b) => parseFloat(a) + parseFloat(b), 0)
+    let nonStringNums = x.filter((e) => typeof e === 'number').reduce((a, b) => a + b, 0)
+    return nonStringNums - stringNums
+}
