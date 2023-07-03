bc3cadc27790b0a5fc1c2a76bb7fcfdc0f3b0be2
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-07-03 13:28:21 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Most digits.js
+++ Codewars/Javascript/7_kyu/Most digits.js
@@ -4,3 +4,15 @@
 // If two numbers in the argument array have the same number of digits, return the first one in the array.
 
 // My Solution:
+function findLongest(array) {
+    let res = Number.NEGATIVE_INFINITY
+    let longuestSoFar = 0
+    for (let a of array) {
+        let strNumLen = a.toString().length
+        if (strNumLen > longuestSoFar) {
+            res = a
+            longuestSoFar = strNumLen
+        }
+    }
+    return res
+}
