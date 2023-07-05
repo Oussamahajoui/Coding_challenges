4b7362229b589822a7757dcbf71777d5e518dfda
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-07-05 11:14:08 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Sort arrays - 1.js
+++ Codewars/Javascript/7_kyu/Sort arrays - 1.js
@@ -2,3 +2,8 @@
 // Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
 
 // My Solution:
+// input: names - unsorted array
+// output: sorted array
+sortme = function (names) {
+    return names.sort()
+}
