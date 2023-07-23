86b5a791bce6ca3115956128b0d679d0bd79175a
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-07-23 11:00:16 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Sorted yes  no  how .js
+++ Codewars/Javascript/7_kyu/Sorted yes  no  how .js
@@ -7,3 +7,14 @@
 // You can assume the array will always be valid, and there will always be one correct answer.
 
 // My Solution:
+function isSortedAndHow(array) {
+    let ascArray = [...array].sort((a, b) => a - b)
+    let descArray = [...array].sort((a, b) => b - a)
+    if (array.every((val, index) => val === ascArray[index])) {
+        return "yes, ascending"
+    } else if (array.every((val, index) => val === descArray[index])) {
+        return "yes, descending"
+    } else {
+        return "no"
+    }
+}
