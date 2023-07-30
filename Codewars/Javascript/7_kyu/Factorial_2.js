4ed30eb3882d12037b7d5eb2d948ef3b18ff0c05
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-07-30 10:22:41 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Factorial_2.js
+++ Codewars/Javascript/7_kyu/Factorial_2.js
@@ -4,3 +4,17 @@
 // https://en.wikipedia.org/wiki/Factorial
 
 // My Solution:
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1
+    }
+    let res = 1
+    while (n > 0) {
+        res *= n
+        n -= 1
+    }
+    return res
+}
+
+//Simpler way:
+const factorial = n => n ? factorial(n - 1) * n : 1;
