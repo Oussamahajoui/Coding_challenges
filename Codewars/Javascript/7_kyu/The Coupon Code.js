1729bd6cab2aec4c450708a3d8364eed44dbdb3c
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-08-07 11:11:45 -0400

    added my solution

--- Codewars/Javascript/7_kyu/The Coupon Code.js
+++ Codewars/Javascript/7_kyu/The Coupon Code.js
@@ -13,3 +13,6 @@
 // checkCoupon("123", "123", "July 9, 2015", "July 2, 2015")  ===  false
 
 // My Solution:
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+    return enteredCode === correctCode && Date.parse(expirationDate) >= Date.parse(currentDate)
+}
