05ac2c93c4390f331ec1a5695d50f66e3a4ef0f0
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-08-08 11:07:02 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Check the exam.js
+++ Codewars/Javascript/7_kyu/Check the exam.js
@@ -15,3 +15,16 @@
 //  ["b", "c", "b", "a"]   ["" , "a", "a", "c"]  →     0
 
 // My Solution:
+function checkExam(array1, array2) {
+    let score = 0
+    for (let i = 0; i < array1.length; i++) {
+        if (array1[i] === array2[i]) {
+            score += 4
+        } else if (array2[i] === '') {
+            score += 0
+        } else {
+            score -= 1
+        }
+    }
+    return score > 0 ? score : 0
+}
