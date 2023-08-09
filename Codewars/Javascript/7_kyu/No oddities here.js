ca63a4b047587044fb59bb1066db57b82ff09016
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-08-09 11:01:52 -0400

    added my solution

--- Codewars/Javascript/7_kyu/No oddities here.js
+++ Codewars/Javascript/7_kyu/No oddities here.js
@@ -4,3 +4,6 @@
 // All values in the array will be integers. Return the good values in the order they are given.
 
 // My Solution:
+function noOdds(values) {
+    return values.filter((e) => e % 2 === 0)
+}
