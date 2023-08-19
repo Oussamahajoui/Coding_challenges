7784d3f7193e8ea0fd12ec3ef6548f4f98c0d96e
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-08-19 10:13:56 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Remove anchor from URL.js
+++ Codewars/Javascript/7_kyu/Remove anchor from URL.js
@@ -6,3 +6,12 @@
 // "www.codewars.com?page=1" -->"www.codewars.com?page=1"
 
 // My Solution:
+function removeUrlAnchor(url) {
+    const idx = url.indexOf('#')
+    return idx === -1 ? url : url.slice(0, idx)
+}
+
+// Other way:
+function removeUrlAnchor(url) {
+    return url.split('#')[0];
+}
