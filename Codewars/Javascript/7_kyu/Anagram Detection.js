66d0fb85841eca90a0dc213a2f4caf2a1497ddb6
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-08-21 10:08:12 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Anagram Detection.js
+++ Codewars/Javascript/7_kyu/Anagram Detection.js
@@ -11,3 +11,44 @@
 // "Buckethead" is an anagram of "DeathCubeK"
 
 // My Solution:
+// write the function isAnagram
+var isAnagram = function (test, original) {
+    let myCounter = {}
+    for (let c of original) {
+        c = c.toLowerCase()
+        if (c in myCounter) {
+            myCounter[c] += 1
+        } else {
+            myCounter[c] = 1
+        }
+    }
+    let testCounter = {}
+    for (let c of test) {
+        c = c.toLowerCase()
+        if (c in testCounter) {
+            testCounter[c] += 1
+        } else {
+            testCounter[c] = 1
+        }
+    }
+    const keys1 = Object.keys(myCounter)
+    const keys2 = Object.keys(testCounter)
+
+    if (keys1.length != keys2.length) {
+        return false
+    }
+
+    for (const key of keys1) {
+        if (myCounter[key] !== testCounter[key]) {
+            return false
+        }
+    }
+    return true
+};
+
+// Simpler using sort:
+const isAnagram = function (test, original) {
+    const t = test.toLowerCase().split('').sort().join('');
+    const o = original.toLowerCase().split('').sort().join('');
+    return (t == o) ? true : false;
+};
