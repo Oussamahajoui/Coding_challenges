b11a81b613ee367ef60d8d026a9140473f744b9f
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-08-29 11:52:09 -0400

    added my solution

--- Codewars/Javascript/7_kyu/The highest profit wins!.js
+++ Codewars/Javascript/7_kyu/The highest profit wins!.js
@@ -13,3 +13,6 @@
 // All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
 
 // My Solution:
+function minMax(arr) {
+    return [Math.min(...arr), Math.max(...arr)];
+}
