d07d704c4d07e68cb6ecbafc4274dfbece5052a8
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-08-30 11:17:32 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Reverse words.js
+++ Codewars/Javascript/7_kyu/Reverse words.js
@@ -6,3 +6,21 @@
 // "double  spaces"      ==> "elbuod  secaps"
 
 // My Solution:
+function reverseWords(str) {
+    function reverseWord(s) {
+        return s.split('').reverse().join('')
+    }
+    let words = str.split(' ')
+    let res = ''
+    for (let w of words) {
+        res += ' ' + reverseWord(w)
+    }
+    return res.slice(1)
+}
+
+// Other way:
+function reverseWords(str) {
+    return str.split(' ').map(function (word) {
+        return word.split('').reverse().join('');
+    }).join(' ');
+}
