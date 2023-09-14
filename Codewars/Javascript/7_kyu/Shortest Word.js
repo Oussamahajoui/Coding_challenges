288ebe056faaf1356b3cb01416403e5136ddd684
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-09-14 10:10:59 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Shortest Word.js
+++ Codewars/Javascript/7_kyu/Shortest Word.js
@@ -4,3 +4,15 @@
 // String will never be empty and you do not need to account for different data types.
 
 // My Solution:
+function findShort(s) {
+    let res = Number.POSITIVE_INFINITY
+    for (const c of s.split(' ')) {
+        res = Math.min(c.length, res)
+    }
+    return res
+}
+
+// Other way:
+function findShort(s) {
+    return Math.min(...s.split(" ").map(s => s.length));
+}
