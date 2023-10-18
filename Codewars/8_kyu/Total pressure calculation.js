3bee178ce05a5259907cb99560610104ceeade02
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-10-17 22:11:23 -0400

    added today's problem

---
+++ Codewars/8_kyu/Total pressure calculation.js
@@ -0,0 +1,6 @@
+// Description:
+// Given the molecular mass of two molecules M1 and M 2, their masses present
+// m1 and m2 in a vessel of volume V at a specific temperature T, find the total pressure
+// P total exerted by the molecules.
+
+// My Solution:
