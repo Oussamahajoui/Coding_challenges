c56d42e159ae3a014a0b3b1b2e7190a6b7433eae
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-10-17 22:12:57 -0400

    added my solution

--- Codewars/8_kyu/Total pressure calculation.js
+++ Codewars/8_kyu/Total pressure calculation.js
@@ -4,3 +4,11 @@
 // P total exerted by the molecules.
 
 // My Solution:
+solution = (M1, M2, m1, m2, V, T) => {
+    M1 = m1 * 0.001 / M1;
+    M2 = m2 * 0.001 / M2;
+    T = T + 273.15;
+    const R = 0.082;
+
+    return (((M1 + M2) * R * T) / V) * 1000;
+}
