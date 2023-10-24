36d0758f689ab5453c706b8c32f0eb7846ebcd40
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-10-23 21:43:41 -0400

    added my solution

--- Codewars/8_kyu/Simple Fun #352 Reagent Formula.js
+++ Codewars/8_kyu/Simple Fun #352 Reagent Formula.js
@@ -33,3 +33,12 @@
 // Happy Coding ^_^
 
 // My Solution:
+function isValid(formula) {
+    const firstRule = !(formula.includes(1) && formula.includes(2))
+    const secondRule = !(formula.includes(3) && formula.includes(4))
+    const thirdRule = formula.includes(5) === formula.includes(6)
+    const fourthRule = formula.includes(7) || formula.includes(8)
+
+    return firstRule && secondRule && thirdRule && fourthRule
+
+}
