99649acbe824cc985ceb98ea91c583cc5d26ed7b
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-10-27 21:22:03 -0400

    added my solution

--- Codewars/8_kyu/Simple Comparison.js
+++ Codewars/8_kyu/Simple Comparison.js
@@ -4,3 +4,6 @@
 // To make this challange harder and to promp the challenger to read up about coercion I have disabled some of the built in methods including .toString(), .join(), .split(), parseInt and .Number().
 
 // My Solution:
+function add(a, b) {
+    return a == b
+}
