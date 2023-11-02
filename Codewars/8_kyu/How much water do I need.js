b40fdf93f88bf6f53f8e0ce1fbd834630af51a3b
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-11-02 17:25:22 -0400

    added my solution

--- Codewars/8_kyu/How much water do I need.js
+++ Codewars/8_kyu/How much water do I need.js
@@ -10,3 +10,14 @@
 // The answer should be rounded to the nearest 2 decimal places.
 
 // My Solution:
+function howMuchWater(water, load, clothes) {
+    if (clothes > 2 * load) {
+        return "Too much clothes"
+    }
+    else if (clothes < load) {
+        return "Not enough clothes"
+    }
+    else {
+        return parseFloat((water * (1.1 ** (clothes - load))).toFixed(2))
+    }
+}
