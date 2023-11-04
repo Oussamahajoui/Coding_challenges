62471e2737e14077d22d06d307c0beb8844d377f
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-11-04 11:31:47 -0400

    added my solution

--- Codewars/8_kyu/Training JS #13 Number object and its properties.js
+++ Codewars/8_kyu/Training JS #13 Number object and its properties.js
@@ -17,3 +17,11 @@
 // What you need to think about is how to judge it correctly and effectively and don't forget isNaN().
 
 // My Solution:
+function whatNumberIsIt(n) {
+    if (n === Infinity) return "Input number is Number.POSITIVE_INFINITY";
+    else if (n === -Infinity) return "Input number is Number.NEGATIVE_INFINITY";
+    else if (n == 1.7976931348623157e+308) return "Input number is Number.MAX_VALUE";
+    else if (isNaN(n)) return "Input number is Number.NaN";
+    else if (n == 5e-324) return "Input number is Number.MIN_VALUE";
+    else return ("Input number is " + n);
+}
