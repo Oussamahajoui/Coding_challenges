29e9afed3c71b882aa49a2e5247363998223ffff
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-11-05 10:27:18 -0500

    added my solution

--- Codewars/8_kyu/Be Concise I - The Ternary Operator.js
+++ Codewars/8_kyu/Be Concise I - The Ternary Operator.js
@@ -16,3 +16,6 @@
 // Whatever you do, do not change what the function does. Good luck :)
 
 // My Solution:
+function describeAge(a) {
+    return `You're a(n) ${a <= 12 ? 'kid' : a <= 17 ? 'teenager' : a <= 64 ? 'adult' : 'elderly'}`
+}
