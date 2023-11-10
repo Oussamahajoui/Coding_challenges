441364165a86c4c873277e4c60145b3896bea380
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-11-09 20:08:05 -0500

    added my solution

--- Codewars/8_kyu/Training JS #14 Methods of Number object--toString() and toLocaleString().js
+++ Codewars/8_kyu/Training JS #14 Methods of Number object--toString() and toLocaleString().js
@@ -13,3 +13,12 @@
 // That's all of your work. My work is print your color code on your screen.
 
 // My Solution:
+function colorOf(r, g, b) {
+    r = r.toString(16)
+    g = g.toString(16)
+    b = b.toString(16)
+    if (r.length < 2) { r = "0" + r };
+    if (g.length < 2) { g = "0" + g };
+    if (b.length < 2) { b = "0" + b };
+    return '#' + r + g + b
+}
