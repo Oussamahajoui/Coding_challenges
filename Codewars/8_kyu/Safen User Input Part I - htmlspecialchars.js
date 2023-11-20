de3439300f61cfface5062b7bf3b1621e4396cb1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-11-20 17:13:53 -0500

    added my solution

--- Codewars/8_kyu/Safen User Input Part I - htmlspecialchars.js
+++ Codewars/8_kyu/Safen User Input Part I - htmlspecialchars.js
@@ -12,3 +12,10 @@
 // Good luck :D
 
 // My Solution:
+function htmlspecialchars(formData) {
+    formData = formData.replace(/&/g, '&amp;')
+    formData = formData.replace(/</g, '&lt;')
+    formData = formData.replace(/>/g, '&gt;')
+    formData = formData.replace(/"/g, '&quot;')
+    return formData
+}
