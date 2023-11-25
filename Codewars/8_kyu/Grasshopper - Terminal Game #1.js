616efae7d91b208446c4adb3922fc9f199094ecf
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-11-25 16:35:03 -0500

    added my solution

--- Codewars/8_kyu/Grasshopper - Terminal Game #1.js
+++ Codewars/8_kyu/Grasshopper - Terminal Game #1.js
@@ -10,3 +10,10 @@
 // experience	0
 
 // My Solution:
+function Hero(name = 'Hero') {
+    this.name = name
+    this.position = '00'
+    this.health = 100
+    this.damage = 5
+    this.experience = 0
+}
