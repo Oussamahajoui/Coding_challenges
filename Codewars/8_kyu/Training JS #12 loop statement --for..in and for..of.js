0a6d9d5cd058bd5034fbf5c4c5d4a0de7cbc79a7
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-12-05 14:28:05 -0500

    added my solution

--- Codewars/8_kyu/Training JS #12 loop statement --for..in and for..of.js
+++ Codewars/8_kyu/Training JS #12 loop statement --for..in and for..of.js
@@ -8,3 +8,29 @@
 // You should use for..in in your code, otherwise your solution may not pass this kata.
 
 // My Solution:
+function giveMeFive(obj) {
+    let res = []
+    for (const key in obj) {
+        if (key.length === 5 && obj[key].length === 5) {
+            res.push(key)
+            res.push(obj[key])
+        }
+        else if (key.length === 5) {
+            res.push(key)
+        }
+        else if (obj[key].length === 5) {
+            res.push(obj[key])
+        }
+    }
+    return res
+}
+
+// Better way to write it:
+function giveMeFive(obj) {
+    let res = [];
+    for (var key in obj) {
+        if (key.length === 5) res.push(key);
+        if (obj[key].length === 5) res.push(obj[key]);
+    }
+    return res;
+}
