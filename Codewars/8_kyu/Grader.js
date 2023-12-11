fa009dadf18fb7ae8ee2c9a35122070611f6a240
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-12-11 13:41:34 -0500

    added my solution

--- Codewars/8_kyu/Grader.js
+++ Codewars/8_kyu/Grader.js
@@ -17,3 +17,12 @@
 // grader(0.6) should be "D"
 
 // My Solution:
+function grader(score) {
+    switch (true) {
+        case score > 1 || score < 0.6: return "F"
+        case score >= 0.9: return "A"
+        case score >= 0.8: return "B"
+        case score >= 0.7: return "C"
+        case score >= 0.6: return "D"
+    }
+}
