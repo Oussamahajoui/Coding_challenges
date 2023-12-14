ff583d40fe439674cb2172f7ce591dbc2be8cff3
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-12-14 09:13:50 -0500

    added my solution

--- Codewars/8_kyu/Training JS #9 loop statement --while and do..while.js
+++ Codewars/8_kyu/Training JS #9 loop statement --while and do..while.js
@@ -5,3 +5,17 @@
 // n: a number indicating how many times to pad the string.
 
 // My Solution:
+function padIt(str, n) {
+    let arr = str.split('')
+    let count = 1
+    while (count <= n) {
+        if (count % 2 === 1) {
+            arr.unshift("*")
+        }
+        else {
+            arr.push('*')
+        }
+        count++
+    }
+    return arr.join('')
+}
