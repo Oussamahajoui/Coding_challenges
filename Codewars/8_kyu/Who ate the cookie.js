eee716e5c6a4a6d495c83c5b7c36e3fa6114a7ea
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-12-20 19:38:41 -0500

    added my solution

--- Codewars/8_kyu/Who ate the cookie.js
+++ Codewars/8_kyu/Who ate the cookie.js
@@ -8,3 +8,8 @@
 // Please leave feedback for this kata. Cheers!
 
 // My Solution:
+function cookie(x) {
+    return typeof x === 'string' ? "Who ate the last cookie? It was Zach!" :
+        typeof x === 'number' ? "Who ate the last cookie? It was Monica!" :
+            "Who ate the last cookie? It was the dog!"
+}
