cab430d01ba805bf6794b0ff13211c2dd7cf7ba7
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2023-12-30 09:53:25 -0500

    added my solution

--- Codewars/8_kyu/Is there a vowel in there.js
+++ Codewars/8_kyu/Is there a vowel in there.js
@@ -6,3 +6,27 @@
 // Return the resulting array.
 
 // My Solution:
+function isVow(arr) {
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    let vowelsCodes = vowels.map((e) => e.charCodeAt())
+    //   console.log(vowelsCodes)
+    let res = arr.map((e) => {
+        if (vowelsCodes.includes(e)) {
+            return String.fromCharCode(e)
+        }
+        return e
+    })
+    return res
+}
+
+// Other Solution:
+const isVow = a => {
+    let map = {
+        97: 'a',
+        101: 'e',
+        105: 'i',
+        111: 'o',
+        117: 'u',
+    }
+    return a.map(num => map[num] ? map[num] : num);
+}
