2076fafa17629ccc23d5bc40fdc3f8acccac823e
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-03 19:42:50 -0500

    added today's problem

---
+++ Codewars/8_kyu/Geometry Basics Distance between points in 2D.js
@@ -0,0 +1,8 @@
+// Description:
+// This series of katas will introduce you to basics of doing geometry with computers.
+
+// Point objects have attributes x and y.
+
+// Write a function calculating distance between Point a and Point b.
+
+// My Solution:
