589db684ced790f143a53d3f181d0431e9dbc33d
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-04 19:41:51 -0500

    added my solution

--- Codewars/8_kyu/Who is going to pay for the wall.js
+++ Codewars/8_kyu/Who is going to pay for the wall.js
@@ -4,3 +4,9 @@
 // Your code will show Full name of the neighbor and the truncated version of the name as an array. If the number of the characters in name is less than or equal to two, it will return an array containing only the name as is"
 
 // My Solution:
+function whoIsPaying(name) {
+    if (name.length <= 2) {
+        return [name]
+    }
+    return [name, name.slice(0, 2)]
+}
