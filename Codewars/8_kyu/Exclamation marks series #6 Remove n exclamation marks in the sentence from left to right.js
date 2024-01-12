c5386103858e8c67482d4467337602e8d0903839
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-12 17:39:31 -0500

    added my solution

--- Codewars/8_kyu/Exclamation marks series #6 Remove n exclamation marks in the sentence from left to right.js
+++ Codewars/8_kyu/Exclamation marks series #6 Remove n exclamation marks in the sentence from left to right.js
@@ -15,3 +15,18 @@
 // remove("!!!Hi !!hi!!! !hi",100) === "Hi hi hi"
 
 // My Solution:
+function remove(s, n) {
+    let sentence = s.split('')
+    let res = []
+    let i = 0
+    let counter = 0
+    while (i < sentence.length) {
+        if (sentence[i] === '!' && counter < n) {
+            counter += 1
+        } else {
+            res.push(sentence[i])
+        }
+        i++
+    }
+    return res.join('')
+}
