5eb5ec45ea2302eb52e90fb87cd29984b4872364
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-14 17:20:31 -0500

    added my solution

--- Codewars/8_kyu/Leonardo Dicaprio and Oscars.js
+++ Codewars/8_kyu/Leonardo Dicaprio and Oscars.js
@@ -10,3 +10,9 @@
 // if it was over 88 you should return "Leo got one already!"
 
 // My Solution:
+function leo(oscar) {
+    return oscar > 88 ? "Leo got one already!" :
+        oscar === 88 ? "Leo finally won the oscar! Leo is happy" :
+            oscar === 86 ? "Not even for Wolf of wallstreet?!" :
+                "When will you give Leo an Oscar?"
+}
