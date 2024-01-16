4437bad949bf862a1dd7e75a2345ce7e383b4d9c
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-16 16:55:24 -0500

    added my solution

--- Codewars/8_kyu/Quadrants.js
+++ Codewars/8_kyu/Quadrants.js
@@ -17,3 +17,16 @@
 // More on quadrants: Quadrant (plane geometry) - Wikipedia
 
 // My Solution:
+function quadrant(x, y) {
+    if (x > 0 && y > 0) {
+        return 1
+    } else if (x < 0 && y > 0) {
+        return 2
+    }
+    else if (x < 0 && y < 0) {
+        return 3
+    }
+    else {
+        return 4
+    }
+}
