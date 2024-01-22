ad2a598cdeec1d25270e563ff1d7ab4d347bf3bb
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-22 15:26:16 -0500

    added my solution

--- Codewars/8_kyu/Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
+++ Codewars/8_kyu/Exclamation marks series #2 Remove all exclamation marks from the end of sentence.js
@@ -11,3 +11,11 @@
 // "Hi"      ---> "Hi"
 
 // My Solution:
+function remove(string) {
+    let res = string.split('')
+    let i = res.length - 1
+    while (res[i] === '!') {
+        i -= 1
+    }
+    return res.slice(0, i + 1).join('')
+}
