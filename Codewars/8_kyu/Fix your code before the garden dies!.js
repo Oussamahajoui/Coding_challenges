05ffb58ce597d72d83e14ba0530cbccd78cc5c10
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-26 14:51:46 -0500

    added my solution

--- Codewars/8_kyu/Fix your code before the garden dies!.js
+++ Codewars/8_kyu/Fix your code before the garden dies!.js
@@ -4,3 +4,11 @@
 // Your task is to debug the code before your plants die!
 
 // My Solution:
+function rainAmount(mm) {
+    if (mm < 40) {
+        return `You need to give your plant ${40 - mm}mm of water`
+    }
+    else {
+        return "Your plant has had more than enough water for today!"
+    }
+}
