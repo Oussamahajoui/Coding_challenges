787c3d0262a87cb11732650348f368a0fa9000b2
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-01-31 10:52:19 -0500

    added my solution

--- Codewars/8_kyu/Basic subclasses - Adam and Eve.js
+++ Codewars/8_kyu/Basic subclasses - Adam and Eve.js
@@ -4,3 +4,23 @@
 // You have to do God's job. The creation method must return an array of length 2 containing objects (representing Adam and Eve). The first object in the array should be an instance of the class Man. The second should be an instance of the class Woman. Both objects have to be subclasses of Human. Your job is to implement the Human, Man and Woman classes.
 
 // My Solution:
+class God {
+    static create() {
+        return [new Man(), new Woman()];
+    }
+}
+class Human {
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Man extends Human {
+    constructor(name) {
+        super(name);
+    }
+}
+class Woman extends Human {
+    constructor(name) {
+        super(name);
+    }
+}
