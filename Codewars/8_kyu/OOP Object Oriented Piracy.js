f22980f42acda32fc2a2715d5a02d603da9e2b21
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-02-07 10:06:39 -0500

    added my solution

--- Codewars/8_kyu/OOP Object Oriented Piracy.js
+++ Codewars/8_kyu/OOP Object Oriented Piracy.js
@@ -32,3 +32,12 @@
 // Good luck and may you find GOOOLD!
 
 // My Solution:
+class Ship {
+    constructor(draft, crew) {
+        this.draft = draft
+        this.crew = crew
+    }
+    isWorthIt() {
+        return this.draft - 1.5 * this.crew > 20
+    }
+}
