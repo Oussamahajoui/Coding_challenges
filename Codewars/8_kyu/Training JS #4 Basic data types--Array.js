5aede7010c2e9786b7b8c0ac926b8ca67252a8f2
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-02-14 09:42:33 -0500

    added my solution

--- Codewars/8_kyu/Training JS #4 Basic data types--Array.js
+++ Codewars/8_kyu/Training JS #4 Basic data types--Array.js
@@ -27,3 +27,26 @@
 // In the end, click "Submit" to submit your code pass this kata.
 
 // My Solution:
+function getLength(arr) {
+    //return length of arr
+    return arr.length
+}
+function getFirst(arr) {
+    //return the first element of arr
+    return arr[0]
+}
+function getLast(arr) {
+    //return the last element of arr
+    return arr[arr.length - 1]
+}
+function pushElement(arr) {
+    var el = 1;
+    //push el to arr
+    arr.push(el)
+    return arr
+}
+function popElement(arr) {
+    //pop an element from arr
+    arr.pop()
+    return arr
+}
