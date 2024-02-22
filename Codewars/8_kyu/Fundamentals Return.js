608bdb63eec783c1ba8ac8cddd6f125fb29df041
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-02-21 19:41:27 -0500

    added my solution

--- Codewars/8_kyu/Fundamentals Return.js
+++ Codewars/8_kyu/Fundamentals Return.js
@@ -18,3 +18,26 @@
 // Note: All math operations will be: a (operation) b
 
 // My Solution:
+function add(a, b) {
+    return a + b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function mod(a, b) {
+    return a % b
+}
+
+function exponent(a, b) {
+    return a ** b
+}
+
+function subt(a, b) {
+    return a - b
+}
