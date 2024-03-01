d9d0b273f33cd0750bd8ef1d7945293676c88b4b
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-03-01 15:59:27 -0500

    added my solution

--- Codewars/8_kyu/String cleaning.js
+++ Codewars/8_kyu/String cleaning.js
@@ -8,3 +8,11 @@
 // Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact.
 
 // My Solution:
+function stringClean(s) {
+    return s.replace(/\d+/g, '')
+}
+
+//Other solution:
+function stringClean(s) {
+    return s.split('').filter((n) => n != parseInt(n)).join('');
+}
