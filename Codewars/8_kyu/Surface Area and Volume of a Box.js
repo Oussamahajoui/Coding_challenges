2f12c0dd90b615f1685cc2d514eb7551c18f9868
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-03-13 16:04:07 -0400

    added my solution

--- Codewars/8_kyu/Surface Area and Volume of a Box.js
+++ Codewars/8_kyu/Surface Area and Volume of a Box.js
@@ -2,3 +2,8 @@
 // Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 
 // My Solution:
+function getSize(w, h, d) {
+    const area = 2 * w * h + 2 * h * d + 2 * w * d
+    const volume = w * h * d
+    return [area, volume]
+}
