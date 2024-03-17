8d43baf0699b018c84667f4df8dc2bec340e1ce6
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-03-17 11:25:44 -0400

    added my solution

--- Codewars/8_kyu/Welcome to the City.js
+++ Codewars/8_kyu/Welcome to the City.js
@@ -7,3 +7,6 @@
 // This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
 
 // My Solution:
+function sayHello(name, city, state) {
+    return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`
+}
