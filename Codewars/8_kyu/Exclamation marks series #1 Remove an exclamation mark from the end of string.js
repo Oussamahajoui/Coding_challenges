0a12831080534cec927d1c63524e9b9596a07d12
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-03-19 11:15:24 -0400

    added my solution

--- Codewars/8_kyu/Exclamation marks series #1 Remove an exclamation mark from the end of string.js
+++ Codewars/8_kyu/Exclamation marks series #1 Remove an exclamation mark from the end of string.js
@@ -10,3 +10,15 @@
 // "Hi"      ---> "Hi"
 
 // My Solution:
+function remove(string) {
+    if (string[string.length - 1] === '!') {
+        return string.slice(0, string.length - 1)
+    } else {
+        return string
+    }
+}
+
+// Other solution:
+function remove(s) {
+    return s[s.length - 1] == '!' ? s.slice(0, -1) : s;
+}
