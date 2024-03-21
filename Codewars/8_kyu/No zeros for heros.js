116c0ec3870ea8aec118d43d7cb72ebab8df1738
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-03-21 10:47:59 -0400

    added my solution

--- Codewars/8_kyu/No zeros for heros.js
+++ Codewars/8_kyu/No zeros for heros.js
@@ -12,3 +12,21 @@
 // Zero alone is fine, don't worry about it. Poor guy anyway
 
 // My Solution:
+function noBoringZeros(n) {
+    let number = String(n)
+    let arr = number.split('')
+    let i = arr.length - 1
+    while (i >= 0 && arr[i] === '0') {
+        i -= 1
+    }
+    let new_n = arr.slice(0, i + 1)
+    return Number(new_n.join(''))
+}
+
+// Other Solution:
+function noBoringZeros(n) {
+    while (n % 10 === 0 && n !== 0) {
+        n = n / 10
+    }
+    return n
+}
