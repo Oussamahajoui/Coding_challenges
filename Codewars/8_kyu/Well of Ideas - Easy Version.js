85b08903268ce0fd4cc8f0ee0e32054e7742836d
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-04-04 12:53:41 -0400

    added my solution

--- Codewars/8_kyu/Well of Ideas - Easy Version.js
+++ Codewars/8_kyu/Well of Ideas - Easy Version.js
@@ -4,3 +4,13 @@
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
 // My Solution:
+function well(x) {
+    let goodones = x.filter((e) => e === 'good')
+    if (goodones.length > 2) {
+        return "I smell a series!"
+    } else if (goodones.length > 0) {
+        return 'Publish!'
+    } else {
+        return 'Fail!'
+    }
+}
