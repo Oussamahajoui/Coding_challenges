8dbd89bb82991151737eae49e91534f23dbf1b99
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-04-17 11:45:45 -0400

    added my solution

--- Codewars/8_kyu/What's the real floor.js
+++ Codewars/8_kyu/What's the real floor.js
@@ -17,3 +17,14 @@
 // -3  =>  -3
 
 // My Solution:
+function getRealFloor(n) {
+    if (n >= 13) {
+        return n - 2
+    } else if (n < 0) {
+        return n
+    } else if (n == 0) {
+        return 0
+    } else {
+        return n - 1
+    }
+}
