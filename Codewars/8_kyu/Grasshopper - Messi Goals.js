456405b57935121093b4367c4d8aa160c17728ae
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-04-23 10:35:15 -0400

    added my solution

--- Codewars/8_kyu/Grasshopper - Messi Goals.js
+++ Codewars/8_kyu/Grasshopper - Messi Goals.js
@@ -17,3 +17,8 @@
 // Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
 
 // My Solution:
+let laLigaGoals = 43
+let championsLeagueGoals = 10
+let copaDelReyGoals = 5
+
+let totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
