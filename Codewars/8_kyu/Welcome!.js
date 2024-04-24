0f7167aef397ef5bc8ab54ab3daec0900b15fcf7
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-04-24 10:32:47 -0400

    added my solution

--- Codewars/8_kyu/Welcome!.js
+++ Codewars/8_kyu/Welcome!.js
@@ -30,3 +30,49 @@
 // IP_ADDRESS_REQUIRED - no ip address was supplied
 
 // My Solution:
+function greet(language) {
+    switch (language) {
+        case "czech": return "Vitejte"
+        case "danish": return "Velkomst"
+        case "dutch": return "Welkom"
+        case "estonian": return "Tere tulemast"
+        case "finnish": return "Tervetuloa"
+        case "flemish": return "Welgekomen"
+        case "french": return "Bienvenue"
+        case "german": return "Willkommen"
+        case "irish": return "Failte"
+        case "italian": return "Benvenuto"
+        case "latvian": return "Gaidits"
+        case "lithuanian": return "Laukiamas"
+        case "polish": return "Witamy"
+        case "spanish": return "Bienvenido"
+        case "swedish": return "Valkommen"
+        case "welsh": return "Croeso"
+        default: return "Welcome"
+    }
+}
+
+// Other:
+function greet(language) {
+    const GreetingsDB = {
+        english: 'Welcome',
+        czech: 'Vitejte',
+        danish: 'Velkomst',
+        dutch: 'Welkom',
+        estonian: 'Tere tulemast',
+        finnish: 'Tervetuloa',
+        flemish: 'Welgekomen',
+        french: 'Bienvenue',
+        german: 'Willkommen',
+        irish: 'Failte',
+        italian: 'Benvenuto',
+        latvian: 'Gaidits',
+        lithuanian: 'Laukiamas',
+        polish: 'Witamy',
+        spanish: 'Bienvenido',
+        swedish: 'Valkommen',
+        welsh: 'Croeso'
+    },
+        defaultLanguage = 'english';
+    return GreetingsDB[language] || GreetingsDB[defaultLanguage];
+}
