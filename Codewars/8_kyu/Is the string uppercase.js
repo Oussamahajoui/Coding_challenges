215ab9836f2b7e565a698bc901e581fade83ada3
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-04-30 10:05:57 -0400

    added my solution

--- Codewars/8_kyu/Is the string uppercase.js
+++ Codewars/8_kyu/Is the string uppercase.js
@@ -13,3 +13,4 @@
 // In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 
 // My Solution:
+String.prototype.isUpperCase = function () { return this == this.toUpperCase() }
