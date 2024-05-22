2bc26e73239ce62730306991856651316d312794
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-05-22 09:45:41 -0400

    added my solution

--- Codewars/8_kyu/Transportation on vacation.js
+++ Codewars/8_kyu/Transportation on vacation.js
@@ -8,3 +8,12 @@
 // Write a code that gives out the total amount for different days(d).
 
 // My Solution:
+function rentalCarCost(d) {
+    if (d >= 7) {
+        return d * 40 - 50
+    } else if (d >= 3) {
+        return d * 40 - 20
+    } else {
+        return d * 40
+    }
+}
