f045d3951377bb0453f30171a919ffffb539ed6b
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-05-24 11:06:08 -0400

    added my solution

--- Codewars/8_kyu/Rock Paper Scissors!.js
+++ Codewars/8_kyu/Rock Paper Scissors!.js
@@ -9,3 +9,31 @@
 // "paper", "paper" --> "Draw!"
 
 // My Solution:
+function rps(p1, p2) {
+    if ((p1 === 'scissors' && p2 === 'paper')
+        || (p1 === 'paper' && p2 === 'rock')
+        || (p1 === 'rock' && p2 === 'scissors')) {
+        return "Player 1 won!"
+    } else if ((p2 === 'scissors' && p1 === 'paper')
+        || (p2 === 'paper' && p1 === 'rock')
+        || (p2 === 'rock' && p1 === 'scissors')) {
+        return "Player 2 won!"
+    } else {
+        return "Draw!"
+    }
+};
+
+//Other solution:
+const rps = (p1, p2) => {
+    if (p1 == p2)
+        return 'Draw!';
+
+    if (p1 == 'rock' && p2 == 'scissors')
+        return 'Player 1 won!'
+    else if (p1 == 'scissors' && p2 == 'paper')
+        return 'Player 1 won!'
+    else if (p1 == 'paper' && p2 == 'rock')
+        return 'Player 1 won!'
+    else
+        return 'Player 2 won!';
+};
