5e2517c36fc4def6fa26bfb4f1ab5973a5f7f48c
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-05-25 11:16:51 -0400

    added my solution

--- Codewars/8_kyu/Reversed sequence.js
+++ Codewars/8_kyu/Reversed sequence.js
@@ -4,3 +4,11 @@
 // Example : n=5 --> [5,4,3,2,1]
 
 // My Solution:
+const reverseSeq = n => {
+    let res = []
+    while (n >= 1) {
+        res.push(n)
+        n -= 1
+    }
+    return res;
+};
