542497529523963da9b1f20ee1e9dee350d343a1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-05-29 09:56:18 -0400

    added my solution

--- Codewars/8_kyu/Calculate BMI.js
+++ Codewars/8_kyu/Calculate BMI.js
@@ -10,3 +10,23 @@
 // if bmi > 30 return "Obese"
 
 // My Solution:
+function bmi(weight, height) {
+    let bmi = weight / height ** 2
+    if (bmi <= 18.5) {
+        return "Underweight"
+    } else if (bmi <= 25) {
+        return "Normal"
+    } else if (bmi <= 30) {
+        return "Overweight"
+    } else {
+        return "Obese"
+    }
+}
+
+// Other solution:
+function bmi(weight, height) {
+
+    const bmi = weight / (height * height);
+
+    return bmi < 18.5 ? "Underweight" : bmi <= 25 ? "Normal" : bmi <= 30 ? "Overweight" : "Obese";
+}
