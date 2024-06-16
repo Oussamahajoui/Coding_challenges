b0186b10a7a0539da125218cfab8470da9876f34
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-06-16 10:55:54 -0400

    added my solution

--- Codewars/8_kyu/Return_Negative.js
+++ Codewars/8_kyu/Return_Negative.js
@@ -10,3 +10,7 @@
 // Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
 
 //My Solution
+
+function makeNegative(num) {
+    return (num <= 0) ? num : -num
+}
