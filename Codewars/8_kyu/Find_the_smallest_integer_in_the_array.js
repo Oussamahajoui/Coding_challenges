2cbf1bd970892ef0c4a43ba3a1aa80cfc44ae90b
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-06-21 10:14:34 -0400

    added my solution

--- Codewars/8_kyu/Find_the_smallest_integer_in_the_array.js
+++ Codewars/8_kyu/Find_the_smallest_integer_in_the_array.js
@@ -8,3 +8,10 @@
 // You can assume, for the purpose of this kata, that the supplied array will not be empty.
 
 // My Solution
+function findSmallestInt(arr) {
+    let res = Infinity
+    for (let element of arr) {
+        res = Math.min(element, res)
+    }
+    return res
+}
