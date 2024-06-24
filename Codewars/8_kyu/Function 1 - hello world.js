112afa86a89c18d590e6cb07bc83a815678ba623
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-06-24 09:31:20 -0400

    added my solution

--- Codewars/8_kyu/Function 1 - hello world.js
+++ Codewars/8_kyu/Function 1 - hello world.js
@@ -5,3 +5,6 @@
 // Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
 
 // My Solution
+function greet() {
+    return 'hello world!'
+}
