c69f7379da923bec18b11c060b487febd7631dd6
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-07-07 09:50:52 -0400

    added my solution

--- Codewars/8_kyu/Count_of_positives_/_sum_of_negatives.js
+++ Codewars/8_kyu/Count_of_positives_/_sum_of_negatives.js
@@ -9,3 +9,19 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 // My Solution:
+function countPositivesSumNegatives(input) {
+    if (!input || input.length < 1) {
+        return []
+    }
+    let pos = 0
+    let neg = 0
+    input.forEach((e) => {
+        if (e > 0) {
+            pos += 1
+        } else if (e < 0) {
+            neg += e
+        }
+    })
+    return [pos, neg]
+}
+
