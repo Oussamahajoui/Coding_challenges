5bf65db12b5066c84bbf668af9ec201b439be9b7
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-07-08 10:25:17 -0400

    added my solution

--- Codewars/8_kyu/Fake Binary.js
+++ Codewars/8_kyu/Fake Binary.js
@@ -4,3 +4,13 @@
 // Note: input will never be an empty string
 
 // My Solution:
+function fakeBin(x) {
+    let res = []
+    let arr = x.split('')
+    arr.forEach((e) => e < 5 ? res.push('0') : res.push('1'))
+    return res.join('')
+}
+// Other solution:
+function fakeBin(x) {
+    return x.split('').map(n => n < 5 ? 0 : 1).join('');
+}
