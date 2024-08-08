0c059ff8bdd28cc64be7bdf74701715b9efdde35
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-08-08 10:31:54 -0400

    added my solution

---
+++ Codewars/Javascript/7_kyu/Regex validate PIN code.js
@@ -0,0 +1,17 @@
+// Description:
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+// My Solution:
+function validatePIN(pin) {
+    if (pin.length !== 6 && pin.length !== 4) {
+        return false
+    }
+    return pin.split('').every((e) => e >= '0' && e <= '9')
+}
