65aaa0ab105e35f18669e6b2083be0b14f7f4cb6
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-08-20 10:33:11 -0400

    added my solution

--- Codewars/Javascript/7_kyu/Bumps in the Road.js
+++ Codewars/Javascript/7_kyu/Bumps in the Road.js
@@ -4,3 +4,10 @@
 // Unfortunately for you, your drive is very bumpy! Given a string showing either flat road (_) or bumps (n). If you are able to reach home safely by encountering 15 bumps or less, return Woohoo!, otherwise return Car Dead
 
 // My Solution:
+function bump(x) {
+    let bumps = x.split('').filter((e) => e !== '_').length
+    return bumps <= 15 ? "Woohoo!" : "Car Dead"
+}
+
+// Other way:
+const bump = x => x.split('n').length > 16 ? "Car Dead" : "Woohoo!"
