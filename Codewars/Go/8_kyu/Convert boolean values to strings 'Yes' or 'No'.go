2d78353416fe4d50a345492144b939c53ea6237c
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-08-29 10:29:15 -0400

    added my solution

--- Codewars/Go/8_kyu/Convert boolean values to strings 'Yes' or 'No'.go
+++ Codewars/Go/8_kyu/Convert boolean values to strings 'Yes' or 'No'.go
@@ -1,3 +1,12 @@
 // Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
 
 // My Solution:
+package kata
+
+func BoolToWord(word bool) string {
+  if word == true {
+    return "Yes"
+  } else{
+    return "No"
+  }
+}
