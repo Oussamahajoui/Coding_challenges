fd401e67bb6d906d06a64d3f941ed07887810cf0
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-01 10:23:16 -0400

    added my solution

--- Codewars/Go/8_kyu/Remove First and Last Character.go
+++ Codewars/Go/8_kyu/Remove First and Last Character.go
@@ -2,3 +2,8 @@
 // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry about strings with less than two characters.
 
 // My Solution:
+package kata
+
+func RemoveChar(word string) string {
+  return word[1:len(word)-1]
+}
