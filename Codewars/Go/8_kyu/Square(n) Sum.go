a023a8da1e49c1c8116b07ae4ac2001d1b15e440
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-02 09:55:10 -0400

    added today's solution

--- Codewars/Go/8_kyu/Square(n) Sum.go
+++ Codewars/Go/8_kyu/Square(n) Sum.go
@@ -4,4 +4,22 @@
 // For example, for [1, 2, 2] it should return 9 because  1² + 2² + 2² = 9.
 
 // My Solution:
+package kata
 
+func SquareSum(numbers []int) int {
+    sum := 0
+    for i:= 0; i < len(numbers); i++ {
+      sum += numbers[i] * numbers[i]
+    }
+  return sum
+}
+
+//using range:
+package kata
+
+func SquareSum(nums []int) (res int) {
+    for _, val := range nums {
+      res += val * val
+    }
+    return res
+}
