60e4081f1f8091f32a7b22dcc9cd5d890f705cb0
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-06 10:18:05 -0400

    added my solution

--- Codewars/Go/8_kyu/Function 1 - hello world.go
+++ Codewars/Go/8_kyu/Function 1 - hello world.go
@@ -5,3 +5,8 @@
 // Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
 
 // My Solution:
+package kata // do not remove
+
+func greet() string {
+  return "hello world!"
+}
