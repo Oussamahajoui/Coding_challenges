7563d849c159291ac2fd9e177513fd34e48c1ed0
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-10 10:10:47 -0400

    added today's solution

--- Codewars/Go/8_kyu/Returning Strings.go
+++ Codewars/Go/8_kyu/Returning Strings.go
@@ -4,3 +4,11 @@
 // [Make sure you type the exact thing I wrote or the program may not execute properly]
 
 // My Solution:
+package kata
+
+import "fmt"
+
+func Greet(name string) string {
+  res := fmt.Sprintf("Hello, %s how are you doing today?", name)
+  return res
+}
