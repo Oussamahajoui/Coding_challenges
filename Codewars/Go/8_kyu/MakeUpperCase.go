e34b5291c6084e53b7a016af975b08cd74320347
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-17 10:04:12 -0400

    added my solution

--- Codewars/Go/8_kyu/MakeUpperCase.go
+++ Codewars/Go/8_kyu/MakeUpperCase.go
@@ -2,3 +2,10 @@
 // Write a function which converts the input string to uppercase.
 
 // My Solution:
+package kata
+
+import "strings"
+
+func MakeUpperCase(str string) string {
+    return strings.ToUpper(str)
+}
