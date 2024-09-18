7e7e2bc0d14069e7f93e6ce8805ac37cfba6813d
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-18 10:35:35 -0400

    added today's solution

--- Codewars/Go/8_kyu/Abbreviate a Two Word Name.go
+++ Codewars/Go/8_kyu/Abbreviate a Two Word Name.go
@@ -10,3 +10,21 @@
 // patrick feeney => P.F
 
 // My Solution:
+package kata
+
+import "strings"
+
+func AbbrevName(name string) string{
+  words := strings.Fields(name)
+  return strings.ToUpper(string(words[0][0])) + "." +strings.ToUpper(string(words[1][0])) 
+}
+
+//Other way:
+package kata
+
+import "strings"
+
+func AbbrevName(name string) string{
+  words := strings.Split(name, " ")
+  return strings.ToUpper(string(words[0][0])) + "." + strings.ToUpper(string(words[1][0]))
+}
