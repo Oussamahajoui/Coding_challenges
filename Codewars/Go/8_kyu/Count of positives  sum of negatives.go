9331649252424968aeba081641fdb60335b4ff24
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-22 09:47:24 -0400

    added my solution

--- Codewars/Go/8_kyu/Count of positives  sum of negatives.go
+++ Codewars/Go/8_kyu/Count of positives  sum of negatives.go
@@ -9,3 +9,20 @@
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 // My Solution:
+package kata
+
+func CountPositivesSumNegatives(numbers []int) []int {
+  if len(numbers) == 0 {
+    return []int{}
+  }
+  neg := 0
+  pos := 0
+  for _,n := range numbers{
+    if n < 0 {
+      neg += n
+    }else if n > 0{
+      pos += 1
+    }
+  }
+  return []int{pos,neg}
+}
