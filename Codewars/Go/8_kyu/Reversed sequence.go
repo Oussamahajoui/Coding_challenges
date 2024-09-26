cd0a80a944473d94c4b0e37f7b8e1b72c99eca34
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-26 10:52:03 -0400

    added my solution

--- Codewars/Go/8_kyu/Reversed sequence.go
+++ Codewars/Go/8_kyu/Reversed sequence.go
@@ -4,3 +4,11 @@
 // Example : n=5 --> [5,4,3,2,1]
 
 // My Solution:
+package kata
+
+func ReverseSeq(n int) (res[]int) {
+  for idx:= n; idx > 0; idx--{
+    res = append(res, idx)
+  }
+  return res
+}
