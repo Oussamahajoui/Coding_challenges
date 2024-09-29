c7a472abc968e5cb823b34b41338afd8a8d85ed1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-09-29 10:01:45 -0400

    added my solution

--- Codewars/Go/8_kyu/If you can't sleep, just count sheep!!.go
+++ Codewars/Go/8_kyu/If you can't sleep, just count sheep!!.go
@@ -5,3 +5,13 @@
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
 
 // My Solution:
+package kata
+
+import "fmt"
+
+func countSheep(num int) (res string) {
+  for i:= 1; i <= num;i++{
+    res+=  fmt.Sprintf("%v sheep...", i)
+  }
+  return res
+}
