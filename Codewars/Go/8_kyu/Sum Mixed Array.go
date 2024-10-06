06c42261a2c8b956d32c6cbe3b74cfa60e7383bc
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-06 10:03:55 -0400

    added my solution

--- Codewars/Go/8_kyu/Sum Mixed Array.go
+++ Codewars/Go/8_kyu/Sum Mixed Array.go
@@ -4,3 +4,20 @@
 // Return your answer as a number.
 
 // My Solution:
+package kata
+
+import "strconv"
+
+func SumMix(arr []any) (res int) {
+  for _,num := range arr{
+    switch v := num.(type){
+      case int:
+      res += v
+      case string:
+      if intVal, err := strconv.Atoi(v); err == nil{
+        res += intVal
+      }
+    }
+  }
+  return res
+}
