4e2908f7a41f7f53f7c82518e5aec7393a777037
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-10 10:41:47 -0400

    added my solution

--- Codewars/Go/8_kyu/Keep up the hoop.go
+++ Codewars/Go/8_kyu/Keep up the hoop.go
@@ -7,3 +7,12 @@
 // If he doesn't get 10 hoops, return the string "Keep at it until you get it".
 
 // My Solution:
+package kata
+
+func HoopCount(n int) string {
+	if n >= 10 {
+    return  "Great, now move on to tricks" 
+  } else{
+    return  "Keep at it until you get it"
+  }
+}
