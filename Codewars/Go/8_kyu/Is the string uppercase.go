f2094faa82bc30ca4988b2ed1eef6c27ac9d9b3f
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-15 10:19:11 -0400

    added my solution

--- Codewars/Go/8_kyu/Is the string uppercase.go
+++ Codewars/Go/8_kyu/Is the string uppercase.go
@@ -13,3 +13,30 @@
 // In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no letters at all is trivially considered to be in ALL CAPS.
 
 // My Solution:
+package kata
+
+import "unicode"
+
+type MyString string
+
+
+func (s MyString) IsUpperCase() bool {
+  for _ , c := range s{
+    if unicode.IsLower(c) {
+      return false
+    }
+  }
+  return true
+}
+
+//Other way:
+import (
+	"strings"
+  )
+  
+  type MyString string
+  
+  func (s MyString) IsUpperCase() bool {
+	// Your code here!
+	return s == MyString(strings.ToUpper(string(s)))
+  }
