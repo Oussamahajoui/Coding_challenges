7b961c2f4a38dbe48ab1c3fb2c5e71133d3cf98c
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-16 09:59:16 -0400

    added my solution

--- Codewars/Go/8_kyu/Powers of 2.go
+++ Codewars/Go/8_kyu/Powers of 2.go
@@ -7,3 +7,13 @@
 // n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
 
 // My Solution:
+package kata
+
+import "math"
+
+func PowersOfTwo(n int) (res[]uint64) {
+  for i:= 0; i <=n ; i++ {
+    res = append(res, uint64(math.Pow(2, float64(i))))
+  }
+  return res
+}
