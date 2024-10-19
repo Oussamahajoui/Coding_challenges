d3bc262043a6bf603586b06482e1af7ad5390077
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-19 17:58:20 -0400

    added my solution

--- Codewars/Go/8_kyu/Is it a palindrome.go
+++ Codewars/Go/8_kyu/Is it a palindrome.go
@@ -4,3 +4,18 @@
 // A palindrome is a word, number, phrase, or other sequence of symbols that reads the same backwards as forwards, such as madam or racecar.
 
 // My Solution:
+package kata
+
+import "strings"
+
+func ReverseString(s string) string{
+    runes := []rune(s)
+    for i, j := 0, len(runes) - 1; i <j; i, j = i+1, j-1 {
+      runes[i] , runes[j] = runes[j] , runes[i]
+    }
+    return string(runes)
+  }
+
+func IsPalindrome(str string) bool {
+  return ReverseString(strings.ToLower(str)) == strings.ToLower(str)
+}
