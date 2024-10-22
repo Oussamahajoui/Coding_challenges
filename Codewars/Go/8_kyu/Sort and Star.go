a1cf5466344426463c1e282e45972d17f4f70bca
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-22 10:04:36 -0400

    added my solution

--- Codewars/Go/8_kyu/Sort and Star.go
+++ Codewars/Go/8_kyu/Sort and Star.go
@@ -6,3 +6,20 @@
 // You should not remove or add elements from/to the array.
 
 // My Solution:
+package kata
+
+import (
+  "sort"
+  "strings"
+)
+
+func TwoSort(arr []string) string {
+  sort.Strings(arr)
+  first := arr[0]
+  var parts []string
+  for _, c := range first{
+    parts = append(parts, string(c))
+    parts = append(parts, "***")
+  }
+  return strings.Join(parts[:len(parts) -1], "")
+}
