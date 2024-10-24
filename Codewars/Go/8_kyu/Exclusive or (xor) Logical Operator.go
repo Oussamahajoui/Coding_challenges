fe8f00894495b425a22c01e21dbbec3ca772476c
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-24 10:08:25 -0400

    added my solution

--- Codewars/Go/8_kyu/Exclusive or (xor) Logical Operator.go
+++ Codewars/Go/8_kyu/Exclusive or (xor) Logical Operator.go
@@ -11,3 +11,8 @@
 // Since we cannot define keywords in Javascript (well, at least I don't know how to do it), your task is to define a function xor(a, b) where a and b are the two expressions to be evaluated. Your xor function should have the behaviour described above, returning true if exactly one of the two expressions evaluate to true, false otherwise.
 
 // My Solution:
+package kata
+
+func Xor(a bool, b bool) bool {
+  return a != b
+}
