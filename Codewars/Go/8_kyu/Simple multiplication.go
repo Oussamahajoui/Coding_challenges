4a4fbef6397734f7f273d7d902da1ff071652ab7
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-26 10:00:30 -0400

    added my solution

--- Codewars/Go/8_kyu/Simple multiplication.go
+++ Codewars/Go/8_kyu/Simple multiplication.go
@@ -2,3 +2,12 @@
 // This kata is about multiplying a given number by eight if it is an even number and by nine otherwise.
 
 // My Solution:
+package kata
+
+func SimpleMultiplication(n int) int {
+  if n % 2 == 0 {
+    return n * 8
+  } else {
+    return n * 9
+  }
+}
