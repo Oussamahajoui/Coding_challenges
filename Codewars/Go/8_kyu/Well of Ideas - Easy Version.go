791c599ad1397e63017313618053f68069d58387
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-30 14:59:36 -0400

    added my solution

--- Codewars/Go/8_kyu/Well of Ideas - Easy Version.go
+++ Codewars/Go/8_kyu/Well of Ideas - Easy Version.go
@@ -4,3 +4,20 @@
 // In this kata you need to check the provided array (x) for good ideas 'good' and bad ideas 'bad'. If there are one or two good ideas, return 'Publish!', if there are more than 2 return 'I smell a series!'. If there are no good ideas, as is often the case, return 'Fail!'.
 
 // My Solution:
+package kata
+
+func Well(x []string) string {
+  tmp := []string{}
+  for _, val := range x {
+    if val == "good"{
+      tmp = append(tmp, val)
+    }
+  }
+  if len(tmp) > 2{
+    return "I smell a series!"
+  } else if len(tmp) > 0 {
+    return "Publish!"
+  } else {
+    return "Fail!"
+  }
+}
