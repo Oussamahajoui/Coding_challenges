59260d5baf53418dccd474501a0ad04a96743373
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-10-31 12:39:09 -0400

    added my solution

--- Codewars/Go/8_kyu/Bin to Decimal.go
+++ Codewars/Go/8_kyu/Bin to Decimal.go
@@ -2,3 +2,11 @@
 // Complete the function which converts a binary number (given as a string) to a decimal number.
 
 // My Solution:
+package kata
+
+import "strconv"
+
+func BinToDec(bin string) int {
+  value , _ :=  strconv.ParseInt(bin, 2, 0)
+  return int(value)
+}
