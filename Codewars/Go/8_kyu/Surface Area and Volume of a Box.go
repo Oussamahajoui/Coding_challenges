bc511dfe294f6e31e6913e69f762c0defd1ab427
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-11-01 09:30:09 -0400

    added my solution

--- Codewars/Go/8_kyu/Surface Area and Volume of a Box.go
+++ Codewars/Go/8_kyu/Surface Area and Volume of a Box.go
@@ -2,3 +2,10 @@
 // Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 
 // My Solution:
+package kata
+
+func GetSize(w,h,d int) [2]int {
+  area := 2*w*h + 2*h*d + 2*w*d
+  volume := w * h * d
+  return [2]int{area, volume}
+}
