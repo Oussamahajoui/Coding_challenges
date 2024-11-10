7ecf525303b805d2d0e0323182b27694bddfe5d1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-11-10 09:38:32 -0500

    added today's problem

---
+++ Codewars/Go/8_kyu/Wilson primes.go
@@ -0,0 +1,32 @@
+// Description:
+// Wilson primes satisfy the following condition. Let 
+// P
+// P represent a prime number.
+
+// Then,
+
+// (
+// P
+// −
+// 1
+// )
+// !
+// +
+// 1
+// P
+// ∗
+// P
+// P∗P
+// (P−1)!+1
+// ​
+ 
+// should give a whole number, where 
+// P
+// !
+// P! is the factorial of 
+// P
+// P.
+
+// Your task is to create a function that returns true if the given number is a Wilson prime and false otherwise.
+
+// My Solution:
