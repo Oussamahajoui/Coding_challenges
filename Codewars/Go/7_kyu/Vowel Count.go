a7096a418eddd384e8bdb11fd324d75e75eaabb9
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-11-12 09:16:06 -0500

    added my solution

--- Codewars/Go/7_kyu/Vowel Count.go
+++ Codewars/Go/7_kyu/Vowel Count.go
@@ -6,3 +6,20 @@
 // The input string will only consist of lower case letters and/or spaces.
 
 // My Solution:
+package kata
+
+func GetCount(str string) (count int) {
+  vowels := map[rune]bool{
+		'a': true,
+		'e': true,
+		'i': true,
+		'o': true,
+		'u': true,
+	}
+  for _, c := range str {
+    if vowels[c]{
+      count++
+    }
+  }
+  return count
+}
