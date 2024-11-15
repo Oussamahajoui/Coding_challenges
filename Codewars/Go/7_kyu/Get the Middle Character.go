8d4a83a519a58bbe959f51a468feead73fea8b1f
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-11-15 10:15:30 -0500

    added my solution

--- Codewars/Go/7_kyu/Get the Middle Character.go
+++ Codewars/Go/7_kyu/Get the Middle Character.go
@@ -10,3 +10,28 @@
 // "A" --> "A"
 
 // My Solution:
+package kata
+
+import "math"
+
+func GetMiddle(s string) string {
+  length := len(s)
+  if int(length) % 2 != 0 {
+    d := int(math.Floor(float64(length)/2))
+    return string(s[d])
+  } else{
+    e := int(math.Floor(float64(length)/2)) - 1
+    f := int(math.Floor(float64(length)/2))  + 1
+    return s[e:f]
+  }
+  
+}
+
+// Much simpler way:
+package kata
+
+func GetMiddle(s string) string {
+    n := len(s)
+    if n==0 {return s}
+    return s[(n - 1) / 2 : n / 2 + 1]
+}
