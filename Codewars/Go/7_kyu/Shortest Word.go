6675adf6ee4952fae64d62680809029dbdd862aa
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-11-17 10:29:06 -0500

    added my solution

--- Codewars/Go/7_kyu/Shortest Word.go
+++ Codewars/Go/7_kyu/Shortest Word.go
@@ -4,3 +4,18 @@
 // String will never be empty and you do not need to account for different data types.
 
 // My Solution:
+package kata
+
+import "strings"
+
+func FindShort(s string) (res int) {
+  words := strings.Split(s, " ")
+  res = len(s)
+  for _, word := range words {
+    word_len := len(word)
+    if word_len < res {
+      res = word_len
+    }
+  }
+  return res
+}
