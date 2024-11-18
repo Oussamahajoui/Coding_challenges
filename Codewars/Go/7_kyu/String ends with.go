f07a9078808575fb931725df25266f63f975b5c8
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-11-18 09:41:31 -0500

    added my solution

--- Codewars/Go/7_kyu/String ends with.go
+++ Codewars/Go/7_kyu/String ends with.go
@@ -7,3 +7,12 @@
 // solution("abc", "d") // returns false
 
 // My Solution:
+package kata
+
+func solution(str, ending string) bool {
+  if len(ending) > len(str) {
+    return false
+  }
+  return str[len(str)-len(ending):] == ending
+  
+}
