69bb857242c99cc57040fdd21d08bc79da23ed44
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-11-23 11:21:35 -0500

    added my solution

--- Codewars/Go/7_kyu/Two to One.go
+++ Codewars/Go/7_kyu/Two to One.go
@@ -8,3 +8,31 @@
 
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+//My Solution:
+package kata
+
+import (
+	"sort"
+	"strings"
+)
+
+func TwoToOne(s1 string, s2 string) string {
+  allLetters := []rune(s1 + s2)
+
+	sort.Slice(allLetters, func(i, j int) bool {
+		return allLetters[i] < allLetters[j]
+	})
+
+	var res strings.Builder
+	seen := make(map[rune]bool)
+
+	for _, c := range allLetters {
+		if !seen[c] {
+			seen[c] = true
+			res.WriteRune(c)
+		}
+	}
+
+	return res.String()
+}
