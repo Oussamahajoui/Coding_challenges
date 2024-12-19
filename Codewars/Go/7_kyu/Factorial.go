f7bfa11dbb424eb04f4ff16cf98dae5472756995
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-12-19 14:39:14 -0500

    added my solution

--- Codewars/Go/7_kyu/Factorial.go
+++ Codewars/Go/7_kyu/Factorial.go
@@ -4,3 +4,16 @@
 // https://en.wikipedia.org/wiki/Factorial
 
 // My Solution:
+package kata
+
+func Factorial(n int) int {
+	if n == 0 || n == 1 {
+    return 1
+  }
+  res := 1
+  for n > 0 {
+    res *= n
+    n -= 1
+  }
+  return res
+}
