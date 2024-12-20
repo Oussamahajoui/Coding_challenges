9314811149de798752233578909e637502c1ba93
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-12-20 09:37:43 -0500

    added my solution

--- Codewars/Go/7_kyu/Two fighters, one winner.go
+++ Codewars/Go/7_kyu/Two fighters, one winner.go
@@ -24,3 +24,18 @@
 // }
 
 // My Solution:
+package kata
+
+import "math"
+
+func DeclareWinner(fighter1 Fighter, fighter2 Fighter, firstAttacker string) string {
+  f1 := math.Ceil((float64(fighter1.Health) / float64(fighter2.DamagePerAttack)))
+  f2 := math.Ceil((float64(fighter2.Health)  / float64(fighter1.DamagePerAttack)))
+  if f1 < f2 {
+    return fighter2.Name
+  } else if f2 < f1 {
+    return fighter1.Name
+  } else {
+    return firstAttacker
+  }
+}
