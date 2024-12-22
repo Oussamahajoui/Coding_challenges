9f0f093342f84b1ff237848106d1f5803f8993a1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-12-22 10:30:08 -0500

    added my solution

--- Codewars/Go/7_kyu/Sum of Cubes.go
+++ Codewars/Go/7_kyu/Sum of Cubes.go
@@ -9,3 +9,14 @@
 // 3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
 
 // My Solution:
+package kata
+
+import "math"
+
+func SumCubes(n int) int {
+	var res int
+  for i:= 1.00; i <= float64(n); i++ {
+    res += int(math.Pow(i,3))
+  }
+  return res
+}
