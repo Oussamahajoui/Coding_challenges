441bf3f2a2a4c54881472d2d2665c6cb15379c6d
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-12-28 12:48:06 -0500

    added my solution

--- Codewars/Go/7_kyu/Speed Control.go
+++ Codewars/Go/7_kyu/Speed Control.go
@@ -21,3 +21,23 @@
 // Happy coding!
 
 // My Solution:
+package kata
+
+import "math"
+
+func Gps(s int, x []float64) int {
+    if len(x) <= 1 {
+      return 0
+    }
+  var maxSpeed int
+  for i := 0; i < len(x) - 1; i++ {
+    section := x[i+1] - x[i]
+    val := (3600 * section) / float64(s)
+    speed := int(math.Floor(val))
+    if speed > maxSpeed {
+      maxSpeed = speed
+    }
+  }
+  
+  return maxSpeed
+}
