2d6ddccbee32c66857400fd11a4186c4e319c722
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2024-12-30 10:48:10 -0500

    added my solution

--- Codewars/Go/7_kyu/Simple remove duplicates.go
+++ Codewars/Go/7_kyu/Simple remove duplicates.go
@@ -14,3 +14,36 @@
 // Good luck!
 
 // My Solution:
+package kata 
+
+func Solve(arr []int) []int {
+	mySet := make(map[int]struct{})
+  var res []int
+  
+  for i:= len(arr)- 1; i >= 0; i-- {
+    if _, exists := mySet[arr[i]]; !exists{
+      mySet[arr[i]] = struct{}{}
+      res = append(res, arr[i])
+    }
+  }
+  
+  for i,j := 0 , len(res) - 1; i <j ; i, j = i + 1, j - 1 {
+    res[i], res[j] = res[j] , res[i]
+  }
+  
+  return res
+}
+
+//Other way:
+func Solve(arr []int) (res []int) {
+    visited := map[int]bool{}
+    for i := len(arr) - 1; i >= 0; i-- {
+        n := arr[i]
+        if visited[n] { continue }
+      
+        visited[n] = true
+        res = append([]int{n}, res...)
+    }
+  
+    return
+}
