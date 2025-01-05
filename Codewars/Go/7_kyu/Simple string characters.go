7e5e42e34500ad0ad4075d51154679647b799e3c
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2025-01-05 09:12:41 -0500

    added my solution

--- Codewars/Go/7_kyu/Simple string characters.go
+++ Codewars/Go/7_kyu/Simple string characters.go
@@ -9,3 +9,40 @@
 // Good luck!
 
 // My Solution:
+package kata
+
+import "regexp"
+
+func Solve(s string) []int {
+	// Count uppercase letters
+	upperRegex := regexp.MustCompile(`[A-Z]`)
+	upper := len(upperRegex.FindAllString(s, -1))
+
+	// Count lowercase letters
+	lowerRegex := regexp.MustCompile(`[a-z]`)
+	lower := len(lowerRegex.FindAllString(s, -1))
+
+	// Count digits
+	numRegex := regexp.MustCompile(`[0-9]`)
+	nums := len(numRegex.FindAllString(s, -1))
+
+	// Count special characters
+	special := len(s) - upper - lower - nums
+
+	// Return the result as a slice
+	return []int{upper, lower, nums, special}
+}
+
+// Better way without regex:
+func Solve(s string) []int {
+	r := make([]int, 4)
+	for _, c := range s {
+	  switch {
+		case c >= 'A' && c <= 'Z': r[0]++;
+		case c >= 'a' && c <= 'z': r[1]++;
+		case c >= '0' && c <= '9': r[2]++;
+		default: r[3]++;
+	  }
+	}
+	return r
+  }
