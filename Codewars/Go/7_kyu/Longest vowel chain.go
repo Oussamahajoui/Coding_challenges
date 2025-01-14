5c840120ee2f3e3894feb7fe47046e9bff9ab8d1
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2025-01-14 06:41:35 -0500

    added my solution

--- Codewars/Go/7_kyu/Longest vowel chain.go
+++ Codewars/Go/7_kyu/Longest vowel chain.go
@@ -4,3 +4,30 @@
 // Good luck!
 
 // My Solution:
+package kata
+
+import "math"
+
+func Solve(s string) int {
+  vowels := []rune{'a','e','i','o','u'}
+  res := 0
+  sofar := 0
+  for _, c := range s {
+    if contains(vowels, c){
+      sofar += 1
+      res = int(math.Max(float64(sofar), float64(res)))
+    } else {
+      sofar = 0
+    }
+  }
+  return res
+}
+
+func contains(s []rune, c rune) bool {
+    for _, v := range s {
+        if v == c {
+            return true
+        }
+    }
+    return false
+}
