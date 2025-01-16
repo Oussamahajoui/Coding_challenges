b13f44fdc79b680600827b0338b01388f4c752f0
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2025-01-16 09:59:36 -0500

    added today's problem

---
+++ Codewars/Go/7_kyu/Heron's formula.go
@@ -0,0 +1,6 @@
+// Description:
+// Write function heron which calculates the area of a triangle with sides a, b, and c (x, y, z in COBOL).
+
+// Output should have 2 digits precision.
+
+// My Solution:
