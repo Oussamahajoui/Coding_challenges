cbdffe56d01124384ce214c8a77e0e21ef5ec2a6
Oussamahajoui <83676274+Oussamahajoui@users.noreply.github.com>
2025-01-19 16:36:44 -0500

    added my solution

--- Codewars/Go/7_kyu/Return the closest number multiple of 10.go
+++ Codewars/Go/7_kyu/Return the closest number multiple of 10.go
@@ -13,3 +13,14 @@
 // 40
 
 // My Solution:
+package kata
+
+import "math"
+
+
+func ClosestMultipleOf10(n uint32) uint32 {
+  if n % 10 >= 5 {
+    return uint32(math.Ceil(float64(n)/10) * 10)
+  }
+  return n / 10 * 10
+}
